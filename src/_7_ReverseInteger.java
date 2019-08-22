ecd60707e26fbf31a45ce78f87cc5f39d64117ea
LiFangCoding <fanglihust@gmail.com>
2019-08-22 13:20:06 +0900

    finish 7

--- src/_7_ReverseInteger.java
+++ src/_7_ReverseInteger.java
@@ -18,5 +18,31 @@
  * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
 public class _7_ReverseInteger {
+    /**
+     * O(log10(x)). Because int型整数在十进制表示下最多有10位，对于每一位的计算量是常数级的.
+     * Overflow  [-2^31, 2^31 - 1]
+     */
+    public static int reverse(int n) {
+        int res = 0;
 
+        while (n != 0) {
+            int temp = res * 10 + n % 10;
+            n = n / 10;
+            if (temp / 10 != res) {
+                res = 0;
+                break;
+            }
+            res = temp;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(-12 % 10);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+        System.out.println(Math.abs(-1));
+    }
 }
---
+++ test/_7_ReverseIntegerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _7_ReverseIntegerTest {
+
+    @Test
+    void reverse() {
+        assertEquals(1, _7_ReverseInteger.reverse(1));
+        assertEquals(21, _7_ReverseInteger.reverse(12));
+        assertEquals(1, _7_ReverseInteger.reverse(10));
+        assertEquals(321, _7_ReverseInteger.reverse(123));
+
+        assertEquals(-321, _7_ReverseInteger.reverse(-123));
+        assertEquals(0, _7_ReverseInteger.reverse(Integer.MIN_VALUE));
+    }
+}
