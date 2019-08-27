5849b45c95478a76e8c5a4c7d7bde41e94ab3835
LiFangCoding <fanglihust@gmail.com>
2019-08-27 18:31:40 +0900

    finish 12

---
+++ test/_12_IntegerToRomanTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _12_IntegerToRomanTest {
+    _12_IntegerToRoman test = new _12_IntegerToRoman();
+
+    @Test
+    void intToRoman() {
+        assertEquals("III", test.intToRoman(3));
+        assertEquals("IV", test.intToRoman(4));
+        assertEquals("IX", test.intToRoman(9));
+        assertEquals("LVIII", test.intToRoman(58));
+        assertEquals("MCMXCIV", test.intToRoman(1994));
+    }
+}
