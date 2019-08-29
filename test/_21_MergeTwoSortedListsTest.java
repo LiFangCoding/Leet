63915fb115dec71d6fc744afe08a6ee7636b844f
LiFangCoding <fanglihust@gmail.com>
2019-08-29 22:16:02 +0900

    finish 21

---
+++ test/_21_MergeTwoSortedListsTest.java
@@ -0,0 +1,18 @@
+import common.ListNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _21_MergeTwoSortedListsTest {
+    _21_MergeTwoSortedLists test = new _21_MergeTwoSortedLists();
+
+    @Test
+    void mergeTwoLists() {
+        ListNode l1 =  ListNode.stringToListNode("[1,2,4]");
+        ListNode l2 =  ListNode.stringToListNode("[1,3,4]");
+        assertEquals("1->1->2->3->4->4", test.mergeTwoLists(l1, l2).toString());
+
+    }
+}
