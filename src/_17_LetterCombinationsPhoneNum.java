684a64dd7b9e9f7510ea5dacb6cc3e7496ed59f7
LiFangCoding <fanglihust@gmail.com>
2019-08-29 19:06:19 +0900

    finish 17

---
+++ src/_17_LetterCombinationsPhoneNum.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+ *
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ *
+ * Example:
+ *
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
+public class _17_LetterCombinationsPhoneNum {
+    List<String> res = new ArrayList<String>();
+    String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        /**
+         * when len = 0, res = [] not [""]
+         */
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        getCombinations(digits, 0, "");
+        return res;
+    }
+
+    private void getCombinations(String digits, int index, String path) {
+        if (index == digits.length()) {
+            res.add(path);
+            return;
+        }
+
+        int i = digits.charAt(index) - '0';
+        for (char c : map[i].toCharArray()) {
+            getCombinations(digits, index + 1, path + c);
+        }
+    }
+}
---
+++ test/_17_LetterCombinationsPhoneNumTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _17_LetterCombinationsPhoneNumTest {
+    _17_LetterCombinationsPhoneNum test = new _17_LetterCombinationsPhoneNum();
+
+    @Test
+    void letterCombinations() {
+        String[] res = {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
+
+        assertArrayEquals(res, test.letterCombinations("23").toArray());
+    }
+}
