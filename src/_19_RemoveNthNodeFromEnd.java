fa3a22f11cd50e80f3a289a3ffb13f38f157989a
LiFangCoding <fanglihust@gmail.com>
2019-08-29 20:31:49 +0900

    finish 19

---
+++ src/_19_RemoveNthNodeFromEnd.java
@@ -0,0 +1,51 @@
+import common.ListNode;
+
+/**
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ *
+ * Example:
+ *
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ *
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ *
+ * Given n will always be valid.
+ *
+ * Follow up:
+ *
+ * Could you do this in one pass?
+ */
+public class _19_RemoveNthNodeFromEnd {
+    /**
+     * 5min37
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy, slow = dummy;
+
+        /**
+         * 1 -> 2 -> 3
+         * n = 2
+         * 1 -> 3
+         */
+        while (fast != null && n-- > 0) {
+            fast = fast.next;
+        }
+
+        while(fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
