46c934dc2bfbadcf26fe6b1232ce4365855bc366
LiFangCoding <fanglihust@gmail.com>
2019-10-24 01:00:51 +0900

    finish 25

---
+++ src/_25_ReverseNodesInKGroup.java
@@ -0,0 +1,69 @@
+import common.ListNode;
+
+/**
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ * <p>
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ * <p>
+ * Example:
+ * <p>
+ * Given this linked list: 1->2->3->4->5
+ * <p>
+ * For k = 2, you should return: 2->1->4->3->5
+ * <p>
+ * For k = 3, you should return: 3->2->1->4->5
+ * <p>
+ * Note:
+ * <p>
+ * Only constant extra memory is allowed.
+ * You may not alter the values in the list's nodes, only nodes itself may be changed.
+ */
+public class _25_ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+
+        while (cur != null) {
+            ListNode first = cur.next;
+
+            ListNode end = cur;
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+
+            if (end == null) {
+                break;
+            }
+
+            ListNode next = end.next;
+            reverse(first, end);
+            first.next = next;
+            cur.next = end;
+            cur = first;
+        }
+
+        return dummy.next;
+    }
+
+    /**
+     * 1-2-3
+     * 3-2-1
+     *
+     * @param head
+     * @param tail
+     */
+    private ListNode reverse(ListNode head, ListNode tail) {
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (prev != tail) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}
