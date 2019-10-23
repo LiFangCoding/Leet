3532437c986346a26c36e403a80fa6dd4e4a9e8c
LiFangCoding <fanglihust@gmail.com>
2019-10-23 21:36:19 +0900

    finish 24

---
+++ src/_24_SwapNodesInPairs.java
@@ -0,0 +1,34 @@
+import common.ListNode;
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p>
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ * <p>
+ * <p>
+ * <p>
+ * Example:
+ * <p>
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ */
+public class _24_SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode cur = dummy;
+
+        while (cur != null && cur.next != null && cur.next.next != null) {
+            ListNode first = cur.next;
+            ListNode second = first.next;
+
+            // in order, exchange two nodes
+            cur.next = second;
+            first.next = second.next;
+            second.next = first;
+
+            cur = first;
+        }
+        return dummy.next;
+    }
+}
---
+++ test/_24_SwapNodesInPairsTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class _24_SwapNodesInPairsTest {
+    _24_SwapNodesInPairs test = new _24_SwapNodesInPairs();
+
+    @Test
+    void swapPairs() {
+
+    }
+}
