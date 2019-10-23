7141989c9d25fc08e719d35dc41875dab16cc333
LiFangCoding <fanglihust@gmail.com>
2019-10-24 03:03:10 +0900

    added new problems

--- src/_1_50/_31_NextPermutation.java
+++ src/_1_50/_31_NextPermutation.java
@@ -9,9 +9,26 @@ package _1_50;
  * <p>
  * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
  * <p>
- * 1,2,3 → 1,3,2
- * 3,2,1 → 1,2,3
- * 1,1,5 → 1,5,1
+ * 1,2,3
+ * 1,3,2
+ * 2,1,3
+ * 2,3,1
+ * 3,1,2
+ * 3,2,1
+ *
+ * 1,2,3,4
+ * 1,2,4,3
+ * 1,3,2,4
+ * 1,3,4,2
+ * 1,4,2,3
+ * 1,4,3,2
+ * 2,1,3,4
+ * 2,1,4,3
+ * 2,3,1,4
+ * 2,3,4,1
+ * 2,4,1,3
+ * 2,4,3,1
  */
 public class _31_NextPermutation {
+
 }
---
+++ src/_1_50/_32_LongestValidParentheses.java
@@ -0,0 +1,18 @@
+package _1_50;
+
+/**
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ * Example 2:
+ * <p>
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ */
+public class _32_LongestValidParentheses {
+}
