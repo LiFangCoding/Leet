fa643257af6b99998626a5c2092e1f01ea9f4bd2
LiFangCoding <fanglihust@gmail.com>
2019-10-26 23:28:11 +0900

    finish 35

---
+++ src/_1_50/_35_SearchInsertPosition.java
@@ -0,0 +1,58 @@
+package _1_50;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ * Example 4:
+ * <p>
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ */
+public class _35_SearchInsertPosition {
+    /**
+     * T = O(log n)
+     * S = O(1)
+     * Find the first num that is larger than or equal with val. The index is the res.
+     * If not exist, return array len.
+     */
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return nums.length;
+        }
+    }
+}
