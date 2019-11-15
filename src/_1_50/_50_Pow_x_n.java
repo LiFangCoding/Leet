f956f75b03ad6b3b61f9babca3bd2d0b1af1f117
LiFangCoding <fanglihust@gmail.com>
2019-11-15 20:03:33 +0900

    finish 50

--- src/_1_50/_50_Pow_x_n.java
+++ src/_1_50/_50_Pow_x_n.java
@@ -23,8 +23,13 @@ package _1_50;
  */
 public class _50_Pow_x_n {
   public double myPow(double x, int n) {
+    /**
+     * Be careful with the Integer.MIN_VALUE
+     * -1 * Integer.MIN_VALUE = Integer.MIN_VALUE
+     */
+//    System.out.println("n is " + n);
     if (n < 0) {
-      return (double) 1 / myPow(x, -n);
+      return (1 / (x * myPow(x, -n - 1)));
     }
 
     if (n == 0) {
@@ -34,5 +39,30 @@ public class _50_Pow_x_n {
     } else {
       return Math.pow(myPow(x, n /2), 2) * x;
     }
+  }
+
+  public double myPow_Iterative(double x, int n) {
+    if (n < 0) {
+      return 1 / (x * myPow_Iterative(x, -(n + 1)));
+    }
+
+    double res = 1, tmp = x;
+
+    while (n != 0) {
+      if (n % 2 == 1) {
+        res *= tmp;
+      }
+      tmp *= tmp;
+       n /= 2;
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) {
+    _50_Pow_x_n test = new _50_Pow_x_n();
+    System.out.println(Integer.MIN_VALUE);
+    System.out.println(Integer.MIN_VALUE * -1);
+    System.out.println(test.myPow(1, -2147483648));
   }
 }
