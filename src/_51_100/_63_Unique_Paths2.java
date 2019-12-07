cc3c7c02fdcdbf42938d985f39df3e544dd02aa4
LiFangCoding <fanglihust@gmail.com>
2019-12-07 17:19:51 +0900

    finish 63

---
+++ src/_51_100/_63_Unique_Paths2.java
@@ -0,0 +1,38 @@
+package _51_100;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * <p>
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * <p>
+ * Now consider if some obstacles are added to the grids. How many unique paths would there be?
+ */
+public class _63_Unique_Paths2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+        /**
+         * special case for 0,0
+         */
+        if (obstacleGrid[0][0] == 0) {
+            dp[0][0] = 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
