fbdf5469e0761cc72961cf1024940ff9d7808861
LiFangCoding <fanglihust@gmail.com>
2019-12-07 17:04:05 +0900

    finish 62

---
+++ src/_51_100/_62_Unique_Paths.java
@@ -0,0 +1,39 @@
+package _51_100;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * <p>
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * <p>
+ * How many possible unique paths are there?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation:
+ * From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down
+ * 2. Right -> Down -> Right
+ * 3. Down -> Right -> Right
+ * Example 2:
+ * <p>
+ * Input: m = 7, n = 3
+ * Output: 28
+ */
+public class _62_Unique_Paths {
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) {
+                    f[i][j] = 1;
+                } else {
+                    f[i][j] = f[i - 1][j] + f[i][j - 1];
+                }
+            }
+        }
+
+        return f[m - 1][n - 1];
+    }
+}
