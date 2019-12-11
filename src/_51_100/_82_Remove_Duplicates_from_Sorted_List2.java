c3b6b5b6a4f3458e868fdf17ce135511ef24d8a8
LiFangCoding <fanglihust@gmail.com>
2019-12-11 18:28:55 +0900

    finish 82

---
+++ src/_51_100/_82_Remove_Duplicates_from_Sorted_List2.java
@@ -0,0 +1,48 @@
+package _51_100;
+
+import common.ListNode;
+
+/**
+ * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2->3->3->4->4->5
+ * Output: 1->2->5
+ * Example 2:
+ * <p>
+ * Input: 1->1->1->2->3
+ * Output: 2->3
+ */
+public class _82_Remove_Duplicates_from_Sorted_List2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+
+        while (cur != null && cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int duplicate = cur.next.val;
+                ListNode newNext = cur.next.next;
+                while (newNext != null && newNext.val == duplicate) {
+                    newNext = newNext.next;
+                }
+                cur.next = newNext;
+                /**
+                 * !!! here cannot cur = cur.next. ex,
+                 * input: [1,2,3,3,4,4,5]
+                 * output: [1,2,4,4,5]
+                 * expected: [1,2,5]
+                 */
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
