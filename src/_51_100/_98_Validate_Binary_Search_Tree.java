62adee6ec1d0de81fed18984b7382920115a3289
LiFangCoding <fanglihust@gmail.com>
2019-12-17 18:52:27 +0900

    finish 98 recursion

--- src/_51_100/_98_Validate_Binary_Search_Tree.java
+++ src/_51_100/_98_Validate_Binary_Search_Tree.java
@@ -1,4 +1,25 @@
 package _51_100;
 
+import common.TreeNode;
+
 public class _98_Validate_Binary_Search_Tree {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
+            return true;
+        }
+
+        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
+            return false;
+        }
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
 }
