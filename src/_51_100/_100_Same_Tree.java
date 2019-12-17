df68a3da2ee6097a9d27870f7db119a4ca69ef18
LiFangCoding <fanglihust@gmail.com>
2019-12-17 20:12:30 +0900

    finish 100

---
+++ src/_51_100/_100_Same_Tree.java
@@ -0,0 +1,18 @@
+package _51_100;
+
+import common.TreeNode;
+
+/**
+ * Given two binary trees, write a function to check if they are the same or not.
+ * <p>
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ */
+public class _100_Same_Tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
