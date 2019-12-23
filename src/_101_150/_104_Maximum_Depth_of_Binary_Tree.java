02047b396abdee5d978ec57b8d868ce9a5db806a
LiFangCoding <fanglihust@gmail.com>
2019-12-23 21:11:47 +0900

    finish 104

--- src/_101_150/_104_Maximum_Depth_of_Binary_Tree.java
+++ src/_101_150/_104_Maximum_Depth_of_Binary_Tree.java
@@ -1,5 +1,7 @@
 package _101_150;
 
+import common.TreeNode;
+
 /**
  * Given a binary tree, find its maximum depth.
  * <p>
@@ -19,4 +21,11 @@ package _101_150;
  * return its depth = 3.
  */
 public class _104_Maximum_Depth_of_Binary_Tree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
 }
