7197a33156be5c5cb77d741eea069cb14efeee39
LiFangCoding <fanglihust@gmail.com>
2019-12-30 21:32:30 +0900

    finish 114 and very clever idea

---
+++ src/_101_150/_114_FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,51 @@
+package _101_150;
+
+import common.TreeNode;
+
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
+ * <p>
+ * For example, given the following tree:
+ * <p>
+ * 1
+ * / \
+ * 2   5
+ * / \   \
+ * 3   4   6
+ * The flattened tree should look like:
+ * <p>
+ * 1
+ * \
+ * 2
+ * \
+ * 3
+ * \
+ * 4
+ * \
+ * 5
+ * \
+ * 6
+ */
+public class _114_FlattenBinaryTreetoLinkedList {
+    private TreeNode prev;
+
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (prev != null) {
+            prev.left = null;
+            prev.right = root;
+        }
+
+        prev = root;
+        /**
+         * !! keep the record. since root.right can change
+         */
+        TreeNode right = root.right;
+        flatten(root.left);
+        flatten(right);
+    }
+}
