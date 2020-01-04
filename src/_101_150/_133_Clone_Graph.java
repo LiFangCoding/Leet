4af14c4d66b2b2081f26b035d06d0ce0465d412a
LiFangCoding <fanglihust@gmail.com>
2020-01-05 06:48:45 +0900

    finish 133 with bfs and dfs

--- src/_101_150/_133_Clone_Graph.java
+++ src/_101_150/_133_Clone_Graph.java
@@ -8,8 +8,20 @@ import java.util.*;
  * Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph. Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
  */
 public class _133_Clone_Graph {
-    public Node cloneGraph(Node node) {
-        return bfsClone(node);
+    Map<Node, Node> map;
+
+    public Node cloneGraph(Node src) {
+        if (src == null) {
+            return null;
+        }
+
+        map = new HashMap<>();
+        map.put(src, new Node(src.val, new ArrayList<>()));
+
+        dfs(src);
+
+        return map.get(src);
+//      return bfsClone(src);
     }
 
     private Node bfsClone(Node src) {
@@ -22,15 +34,12 @@ public class _133_Clone_Graph {
          * because the neighbors also need to be cloned.
          * first, only add vertice. later connect with cloned one.
          * !!! res
-         */
-        Node clonedSrc = new Node(src.val, new ArrayList<>());
-        /**
          * map also to record visited
          */
 
         Queue<Node> q = new LinkedList<>();
         q.add(src);
-        map.put(src, clonedSrc);
+        map.put(src, new Node(src.val, new ArrayList<>()));
 
         while (!q.isEmpty()) {
             Node cur = q.remove();
@@ -38,17 +47,27 @@ public class _133_Clone_Graph {
                 if (!map.containsKey(neigh)) {
                     q.add(neigh);
 
-                    Node clonedNeigh = new Node(neigh.val, new ArrayList<>());
-                    map.put(neigh, clonedNeigh);
+                    map.put(neigh, new Node(neigh.val, new ArrayList<>()));
                 }
-                Node clonedCur = map.get(cur);
-                clonedCur.neighbors.add(map.get(neigh));
+                map.get(cur).neighbors.add(map.get(neigh));
             }
         }
 
         /**
          * !!! important return clonedsrc
          */
-        return clonedSrc;
+        return map.get(src);
+    }
+
+    private void dfs(Node src) {
+        for (Node neigh : src.neighbors) {
+            if (!map.containsKey(neigh)) {
+                map.put(neigh, new Node(neigh.val, new ArrayList<>()));
+                map.get(src).neighbors.add(map.get(neigh));
+                dfs(neigh);
+            } else {
+                map.get(src).neighbors.add(map.get(neigh));
+            }
+        }
     }
 }
