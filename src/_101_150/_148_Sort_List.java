8c3b58bf187c5261791583c400c13b3b9aa54022
LiFangCoding <fanglihust@gmail.com>
2020-01-08 20:54:21 +0900

    finish 148

--- src/_101_150/_148_Sort_List.java
+++ src/_101_150/_148_Sort_List.java
@@ -16,6 +16,55 @@ import common.ListNode;
  */
 public class _148_Sort_List {
     public ListNode sortList(ListNode head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = findMiddle(head);
+
+        ListNode rhead = mid.next;
+        mid.next = null;
+
+        ListNode right = sortList(rhead);
+        ListNode left = sortList(head);
+
+        return merge(left, right);
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy, fast = dummy;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+
+        return dummy.next;
     }
 }
---
+++ src/_101_150/_149_Max_Points_on_a_Line.java
@@ -0,0 +1,38 @@
+package _101_150;
+
+/**
+ * Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [[1,1],[2,2],[3,3]]
+ * Output: 3
+ * Explanation:
+ * ^
+ * |
+ * |        o
+ * |     o
+ * |  o
+ * +------------->
+ * 0  1  2  3  4
+ * Example 2:
+ * <p>
+ * Input: [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+ * Output: 4
+ * Explanation:
+ * ^
+ * |
+ * |  o
+ * |     o        o
+ * |        o
+ * |  o        o
+ * +------------------->
+ * 0  1  2  3  4  5  6
+ */
+public class _149_Max_Points_on_a_Line {
+    public int maxPoints(int[][] points) {
+
+    }
+
+
+}
