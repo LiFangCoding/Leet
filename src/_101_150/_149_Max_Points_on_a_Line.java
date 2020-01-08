d4cfe1c65c5cfc90df2d677ae6f185fe9ec554aa
LiFangCoding <fanglihust@gmail.com>
2020-01-08 21:18:34 +0900

    finish 149

--- src/_101_150/_149_Max_Points_on_a_Line.java
+++ src/_101_150/_149_Max_Points_on_a_Line.java
@@ -1,5 +1,8 @@
 package _101_150;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
  * <p>
@@ -30,9 +33,54 @@ package _101_150;
  * 0  1  2  3  4  5  6
  */
 public class _149_Max_Points_on_a_Line {
+    //TODO: need more practice
     public int maxPoints(int[][] points) {
+        // Write your code here
+        if (points == null) {
+            return 0;
+        }
+        int ans = 0;
 
+        for (int i = 0; i < points.length; i++) {
+            Map<String, Integer> slope = new HashMap<>();
+
+            int maxPoints = 0, overlap = 0, vertical = 0;
+
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i][0] == points[j][0]) {
+                    if (points[i][1] == points[j][1]) {
+                        overlap++;
+                    } else {
+                        vertical++;
+                    }
+                    continue;
+                }
+                int dx = points[i][0] - points[j][0];
+                int dy = points[i][1] - points[j][1];
+                int tmp = gcd(dx, dy);
+                dx /= tmp;
+                dy /= tmp;
+                String k = dy + "/" + dx;
+
+                if (!slope.containsKey(k)) {
+                    slope.put(k, 0);
+                }
+                slope.put(k, slope.get(k) + 1);
+                maxPoints = Math.max(maxPoints, slope.get(k));
+            }
+
+            maxPoints = Math.max(maxPoints, vertical);
+            ans = Math.max(ans, maxPoints + overlap + 1);
+        }
+
+        return ans;
     }
 
-
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
 }
