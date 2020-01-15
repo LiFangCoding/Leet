efa2a6f085e3ba1257a1ba1883ee00c3be08056d
LiFangCoding <fanglihust@gmail.com>
2020-01-15 19:09:28 +0900

    finish 200 with bfs and dfs

--- src/_151_200/_200_NumberofIslands.java
+++ src/_151_200/_200_NumberofIslands.java
@@ -1,5 +1,8 @@
 package _151_200;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
  * <p>
@@ -44,7 +47,8 @@ public class _200_NumberofIslands {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+//                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     /**
                      *
                      */
@@ -64,17 +68,57 @@ public class _200_NumberofIslands {
          *
          * Need to be careful the value should be '1', then it can continue
          */
-        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
+        if (x < 0 || x >= m || y < 0 || y >= n) {
             return;
         }
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        if (grid[x][y] != '1') {
+            return;
+        }
 
         grid[x][y] = '0';
 
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
         for (int i = 0; i < dx.length; i++) {
             dfs(grid, x + dx[i], y + dy[i]);
+        }
+    }
+
+    private void bfs(char[][] grid, int x, int y) {
+        if (grid[x][y] != '1') {
+            return;
+        }
+
+        grid[x][y] = '0';
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y));
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        while (!q.isEmpty()) {
+            Node cur = q.remove();
+
+            for (int i = 0; i < 4; i++) {
+                int newx = cur.x + dx[i];
+                int newy = cur.y + dy[i];
+
+                if (newx >= 0 && newx < grid.length && newy >= 0 && newy < grid[0].length && grid[newx][newy] == '1') {
+                    grid[newx][newy] = '0';
+                    q.add(new Node(newx, newy));
+                }
+            }
+        }
+    }
+
+    class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
