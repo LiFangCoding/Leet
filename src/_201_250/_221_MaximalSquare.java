35f90147aaac1e22a7ca877b4b251564e5ec2b6f
LiFangCoding <fanglihust@gmail.com>
2020-01-20 22:53:55 +0900

    add 221 todo

---
+++ src/_201_250/_221_MaximalSquare.java
@@ -0,0 +1,24 @@
+package _201_250;
+
+/**
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * <p>
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * <p>
+ * Output: 4
+ */
+public class _221_MaximalSquare {
+    /**
+     * Need dp
+     */
+    public int maximalSquare(char[][] matrix) {
+        return -1;
+    }
+}
