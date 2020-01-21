e367d04006963e92544df4f5d1bbe63b3d538833
LiFangCoding <fanglihust@gmail.com>
2020-01-21 17:59:52 +0900

    add 224 todo

---
+++ src/_201_250/_224_BasicCalculator.java
@@ -0,0 +1,29 @@
+package _201_250;
+
+/**
+ * Implement a basic calculator to evaluate a simple expression string.
+ * <p>
+ * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "1 + 1"
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: " 2-1 + 2 "
+ * Output: 3
+ * Example 3:
+ * <p>
+ * Input: "(1+(4+5+2)-3)+(6+8)"
+ * Output: 23
+ * Note:
+ * You may assume that the given expression is always valid.
+ * Do not use the eval built-in library function.
+ */
+public class _224_BasicCalculator {
+    //TODO: need time. haojun
+    public int calculate(String s) {
+        return -1;
+    }
+}
