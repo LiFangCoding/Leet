6923f8ba4793c35734470db7338f61c2705ab7f5
LiFangCoding <fanglihust@gmail.com>
2020-01-22 00:38:36 +0900

    finish 234

---
+++ src/_201_250/_233_NumberofDigitOne.java
@@ -0,0 +1,16 @@
+package _201_250;
+
+/**
+ * Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 13
+ * Output: 6
+ * Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+ */
+public class _233_NumberofDigitOne {
+    public int countDigitOne(int n) {
+        return -1;
+    }
+}
