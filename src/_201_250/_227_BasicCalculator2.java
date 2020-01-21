12653e705c6e068233d3ff82ca96ab6aad88d3df
LiFangCoding <fanglihust@gmail.com>
2020-01-21 18:22:52 +0900

    add 227 todo

---
+++ src/_201_250/_227_BasicCalculator2.java
@@ -0,0 +1,35 @@
+package _201_250;
+
+/**
+ * Implement a basic calculator to evaluate a simple expression string.
+ * <p>
+ * The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . The integer division should truncate toward zero.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "3+2*2"
+ * Output: 7
+ * Example 2:
+ * <p>
+ * Input: " 3/2 "
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: " 3+5 / 2 "
+ * Output: 5
+ * Note:
+ * <p>
+ * You may assume that the given expression is always valid.
+ * Do not use the eval built-in library function.
+ */
+public class _227_BasicCalculator2 {
+    /**
+     * need time. Haojun
+     *
+     * @param s
+     * @return
+     */
+    public int calculate(String s) {
+        return -1;
+    }
+}
