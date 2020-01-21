7fe351b7ce87a9778445d41224e1132251372002
LiFangCoding <fanglihust@gmail.com>
2020-01-21 18:14:36 +0900

    finish 226

---
+++ src/_201_250/_226_InvertBinaryTree.java
@@ -0,0 +1,42 @@
+package _201_250;
+
+import common.TreeNode;
+
+/**
+ * Invert a binary tree.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * Output:
+ * <p>
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ * Trivia:
+ * This problem was inspired by this original tweet by Max Howell:
+ * <p>
+ * Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+ */
+public class _226_InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode right = invertTree(root.left);
+        TreeNode left = invertTree(root.right);
+        root.left = left;
+        root.right = right;
+
+        return root;
+    }
+}
