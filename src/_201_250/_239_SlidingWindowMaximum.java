6f584f415e0d3df1976374e7f59118d07ac2cb61
LiFangCoding <fanglihust@gmail.com>
2020-01-25 03:04:31 +0900

    add TODO 239

---
+++ src/_201_250/_239_SlidingWindowMaximum.java
@@ -0,0 +1,33 @@
+package _201_250;
+
+/**
+ * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+ * <p>
+ * Example:
+ * <p>
+ * Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+ * Output: [3,3,5,5,6,7]
+ * Explanation:
+ * <p>
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+ * <p>
+ * Follow up:
+ * Could you solve it in linear time?
+ */
+public class _239_SlidingWindowMaximum {
+    /**
+     * TODO: haojun
+     */
+    public int[] maxSlidingWindow(int[] A, int k) {
+
+    }
+}
