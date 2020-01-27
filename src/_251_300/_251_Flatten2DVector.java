701f4fe44acceded5270bad8e3abacc559c2aadc
LiFangCoding <fanglihust@gmail.com>
2020-01-27 23:53:45 +0900

    add TODO 251

---
+++ src/_251_300/_251_Flatten2DVector.java
@@ -0,0 +1,47 @@
+package _251_300;
+
+/**
+ * Design and implement an iterator to flatten a 2d vector. It should support the following operations: next and hasNext.
+ * <p>
+ *  
+ * <p>
+ * Example:
+ * <p>
+ * Vector2D iterator = new Vector2D([[1,2],[3],[4]]);
+ * <p>
+ * iterator.next(); // return 1
+ * iterator.next(); // return 2
+ * iterator.next(); // return 3
+ * iterator.hasNext(); // return true
+ * iterator.hasNext(); // return true
+ * iterator.next(); // return 4
+ * iterator.hasNext(); // return false
+ *  
+ * <p>
+ * Notes:
+ * <p>
+ * Please remember to RESET your class variables declared in Vector2D, as static/class variables are persisted across multiple test cases. Please see here for more details.
+ * You may assume that next() call will always be valid, that is, there will be at least a next element in the 2d vector when next() is called.
+ *  
+ * <p>
+ * Follow up:
+ * <p>
+ * As an added challenge, try to code it using only iterators in C++ or iterators in Java.
+ */
+public class _251_Flatten2DVector {
+    //TODO:251
+    class Vector2D {
+
+        public Vector2D(int[][] v) {
+
+        }
+
+        public int next() {
+
+        }
+
+        public boolean hasNext() {
+
+        }
+    }
+}
