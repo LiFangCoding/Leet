7fe21c414a26c62bec71e50185bf0a924fa557c4
LiFangCoding <fanglihust@gmail.com>
2020-01-28 23:45:24 +0900

    add TODO 252

---
+++ src/_251_300/_252_Meeting_Room.java
@@ -0,0 +1,20 @@
+package _251_300;
+
+/**
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [[0,30],[5,10],[15,20]]
+ * Output: false
+ * Example 2:
+ * <p>
+ * Input: [[7,10],[2,4]]
+ * Output: true
+ * NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+ */
+public class _252_Meeting_Room {
+    public boolean canAttendMeetings(int[][] intervals) {
+
+    }
+}
