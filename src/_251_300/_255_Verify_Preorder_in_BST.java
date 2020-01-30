1419750d0678648b35cfe1073d2dc7d221fa9d64
LiFangCoding <fanglihust@gmail.com>
2020-01-30 22:12:11 +0900

    add 255 todo

---
+++ src/_251_300/_255_Verify_Preorder_in_BST.java
@@ -0,0 +1,31 @@
+package _251_300;
+
+/**
+ * Given an array of numbers, verify whether it is the correct preorder traversal sequence of a binary search tree.
+ * <p>
+ * You may assume each number in the sequence is unique.
+ * <p>
+ * Consider the following binary search tree: 
+ * <p>
+ * 5
+ * / \
+ * 2   6
+ * / \
+ * 1   3
+ * Example 1:
+ * <p>
+ * Input: [5,2,6,1,3]
+ * Output: false
+ * Example 2:
+ * <p>
+ * Input: [5,2,1,3,6]
+ * Output: true
+ * Follow up:
+ * Could you do it using only constant space complexity?
+ */
+public class _255_Verify_Preorder_in_BST {
+    //TODO: 255 haojun.
+    public boolean verifyPreorder(int[] preorder) {
+        return false;
+    }
+}
