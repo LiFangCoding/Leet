553989d0f68f2d9181eba1a4b2ced662ab141ff9
LiFangCoding <fanglihust@gmail.com>
2020-02-02 02:03:04 +0900

    add 267 TODO

---
+++ src/_251_300/_267_PalindromePermutation2.java
@@ -0,0 +1,21 @@
+package _251_300;
+
+import java.util.List;
+
+/**
+ * Given a string s, return all the palindromic permutations (without duplicates) of it. Return an empty list if no palindromic permutation could be form.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "aabb"
+ * Output: ["abba", "baab"]
+ * Example 2:
+ * <p>
+ * Input: "abc"
+ * Output: []
+ */
+public class _267_PalindromePermutation2 {
+    public List<String> generatePalindromes(String s) {
+        return null;
+    }
+}
