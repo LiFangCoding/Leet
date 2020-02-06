9d6f478eee0b674f3c0a7ffef9c4992fa7fcd124
LiFangCoding <fanglihust@gmail.com>
2020-02-06 21:09:01 +0900

    add 280 TODO

---
+++ src/_251_300/_279_PerfectSquares.java
@@ -0,0 +1,34 @@
+package _251_300;
+
+/**
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ * Example 2:
+ * <p>
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+public class _279_PerfectSquares {
+    /**
+     * 13
+     * <p>
+     * 4
+     * <p>
+     * 9
+     *
+     * @param n
+     * @return
+     */
+    public int numSquares(int n) {
+
+        int[] f 0] =0;
+
+
+    }
+}
---
+++ src/_251_300/_280_WiggleSort.java
@@ -0,0 +1,15 @@
+package _251_300;
+
+/**
+ * Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+ * <p>
+ * Example:
+ * <p>
+ * Input: nums = [3,5,2,1,6,4]
+ * Output: One possible answer is [3,5,1,6,2,4]
+ */
+public class _280_WiggleSort {
+    public void wiggleSort(int[] A) {
+
+    }
+}
