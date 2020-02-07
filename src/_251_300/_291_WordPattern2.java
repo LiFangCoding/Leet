834504e1ba9f4de3667bcbada7df37eaf04b1244
LiFangCoding <fanglihust@gmail.com>
2020-02-08 00:27:24 +0900

    finish 291

---
+++ src/_251_300/_291_WordPattern2.java
@@ -0,0 +1,28 @@
+package _251_300;
+
+/**
+ * Given a pattern and a string str, find if str follows the same pattern.
+ * <p>
+ * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: pattern = "abab", str = "redblueredblue"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: pattern = pattern = "aaaa", str = "asdasdasdasd"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: pattern = "aabb", str = "xyzabcxzyabc"
+ * Output: false
+ * Notes:
+ * You may assume both pattern and str contains only lowercase
+ */
+public class _291_WordPattern2 {
+    //TODO backtracking
+    public boolean wordPatternMatch(String pattern, String str) {
+        return false;
+    }
+}
