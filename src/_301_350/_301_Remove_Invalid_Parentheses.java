5f4d197fac83c402d49f7f9059b33b6b8da4e7a8
LiFangCoding <fanglihust@gmail.com>
2020-02-23 00:36:57 +0900

    add todo 301

---
+++ src/_301_350/_301_Remove_Invalid_Parentheses.java
@@ -0,0 +1,27 @@
+package _301_350;
+
+import java.util.List;
+
+/**
+ * Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+ * <p>
+ * Note: The input string may contain letters other than the parentheses ( and ).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "()())()"
+ * Output: ["()()()", "(())()"]
+ * Example 2:
+ * <p>
+ * Input: "(a)())()"
+ * Output: ["(a)()()", "(a())()"]
+ * Example 3:
+ * <p>
+ * Input: ")("
+ * Output: [""]
+ */
+public class _301_Remove_Invalid_Parentheses {
+    public List<String> removeInvalidParentheses(String s) {
+        return null;
+    }
+}
