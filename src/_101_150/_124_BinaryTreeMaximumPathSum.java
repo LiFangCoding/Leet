1ccfbebb0cebdc452ae1dd5212f8685caaf89bff
LiFangCoding <fanglihust@gmail.com>
2020-02-26 13:01:08 +0900

    refactor 124

--- src/_101_150/_124_BinaryTreeMaximumPathSum.java
+++ src/_101_150/_124_BinaryTreeMaximumPathSum.java
@@ -29,46 +29,24 @@ import common.TreeNode;
  * Output: 42
  */
 public class _124_BinaryTreeMaximumPathSum {
-    int[] A;
+    int ans;
 
     public int maxPathSum(TreeNode root) {
-        /**
-         * res is A[0]
-         * !!! What if root.val < 0.
-         */
-        A = new int[1];
-        A[0] = root.val;
-
-        helper(root);
-        return A[0];
+        ans = Integer.MIN_VALUE;
+        getOnePathSum(root);
+        return ans;
     }
 
-    /**
-     * Get a path start from node to end.
-     * It cannot have two paths since it needs to connect with parent node.
-     * <p>
-     * We can upate res.
-     * <p>
-     * start from node, the maximum single path.
-     *
-     * @return
-     */
-    private int helper(TreeNode root) {
+    private int getOnePathSum(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        int left = helper(root.left);
-        int right = helper(root.right);
-        if (left < 0) {
-            left = 0;
-        }
+        int l = Math.max(0, getOnePathSum(root.left));
+        int r = Math.max(0, getOnePathSum(root.right));
 
-        if (right < 0) {
-            right = 0;
-        }
-
-        A[0] = Math.max(A[0], root.val + left + right);
-        return root.val + Math.max(left, right);
+        int maxSumFromRoot = root.val + l + r;
+        ans = Math.max(ans, maxSumFromRoot);
+        return root.val + Math.max(l, r);
     }
 }
