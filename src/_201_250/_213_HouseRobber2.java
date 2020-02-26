350517a4fe54dd1d90abcdebc9fd9c4a25773337
LiFangCoding <fanglihust@gmail.com>
2020-02-27 05:42:45 +0900

    finish 213

--- src/_201_250/_213_HouseRobber2.java
+++ src/_201_250/_213_HouseRobber2.java
@@ -1,5 +1,7 @@
 package _201_250;
 
+import java.util.Arrays;
+
 /**
  * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
  * <p>
@@ -24,19 +26,33 @@ public class _213_HouseRobber2 {
             return 0;
         }
 
-        /**
-         * Think about the house i is robbed or not
-         */
-        int n = A.length;
-        if (n == 1) {
+        if (A.length == 1) {
             return A[0];
         }
+        int len = A.length;
+        int ans1 = robOneWay(Arrays.copyOfRange(A, 0, len - 1));
+        int ans2 = robOneWay(Arrays.copyOfRange(A, 1, len));
+        return Math.max(ans1, ans2);
+    }
 
-        if (n == 2) {
-            return Math.max(A[0], A[1]);
+    private int robOneWay(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
         }
 
-        int res = 0;
-        return -1;
+        int len = A.length;
+        int[] f = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            if (i == 0) {
+                f[0] = A[0];
+            } else if (i == 1) {
+                f[1] = Math.max(A[0], A[1]);
+            } else {
+                f[i] = Math.max(f[i - 1], f[i - 2] + A[i]);
+            }
+        }
+
+        return f[len - 1];
     }
 }
