89f2de085504d13d112ce50beea1d1e296bb7f8a
LiFangCoding <fanglihust@gmail.com>
2020-02-29 13:10:25 +0900

    finish 445

--- src/_445_Add_Two_Number2.java
+++ src/_445_Add_Two_Number2.java
@@ -1,5 +1,7 @@
 import common.ListNode;
 
+import java.util.Stack;
+
 /**
  * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
  * <p>
@@ -22,6 +24,46 @@ public class _445_Add_Two_Number2 {
 
         ListNode rHead = addTwoList(rl1, rl2);
         return reverse(rHead);
+    }
+
+    public ListNode addTwoNumbers_stack(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        ListNode cur = null;
+
+        int carry = 0;
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+            int sum = 0;
+
+            if (!s1.isEmpty()) {
+                sum += s1.pop();
+            }
+
+            if (!s2.isEmpty()) {
+                sum += s2.pop();
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            sum %= 10;
+
+            ListNode newHead = new ListNode(sum);
+            newHead.next = cur;
+            cur = newHead;
+        }
+
+        return cur;
     }
 
     private ListNode addTwoList(ListNode l1, ListNode l2) {
