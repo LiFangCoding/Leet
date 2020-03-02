e36cd12287166ccc0dfc193d37e63879c45c9c6d
LiFangCoding <fanglihust@gmail.com>
2020-03-02 15:19:01 +0900

    refactor 98

--- src/_51_100/_98_Validate_Binary_Search_Tree.java
+++ src/_51_100/_98_Validate_Binary_Search_Tree.java
@@ -5,57 +5,86 @@ import common.TreeNode;
 import java.util.Stack;
 
 public class _98_Validate_Binary_Search_Tree {
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
+    class Sol_InRange {
+        public boolean isValidBST(TreeNode root) {
+            return isValidBST(root, null, null);
         }
 
-        return helper(root, null, null);
-    }
+        private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+            if (root == null) {
+                return true;
+            }
 
-    private boolean helper(TreeNode root, Integer min, Integer max) {
-        if (root == null) {
-            return true;
-        }
-
-        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
-            return false;
-        }
-
-        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
-    }
-
-    public boolean isValidBST_iterative(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-
-        Stack<TreeNode> stack = new Stack<>();
-
-        while (root != null) {
-            stack.push(root);
-            root = root.left;
-        }
-
-        TreeNode prev = null;
-        while (!stack.isEmpty()) {
-            TreeNode cur = stack.pop();
-            /**
-             * pay attention, cannot ==.
-             * Need cur > prev
-             */
-            if (prev != null && cur.val <= prev.val) {
+            if (min != null && root.val <= min) {
                 return false;
             }
-            prev = cur;
-            TreeNode node = cur.right;
-            while (node != null) {
-                stack.push(node);
-                node = node.left;
-            }
-        }
 
-        return true;
+            if (max != null && root.val >= max) {
+                return false;
+            }
+
+            boolean l = isValidBST(root.left, min, root.val);
+            if (!l) {
+                return false;
+            }
+            boolean r = isValidBST(root.right, root.val, max);
+            return r;
+        }
     }
 
+    class Sol_inorder_recursion {
+        TreeNode prev;
+
+        public boolean isValidBST(TreeNode root) {
+            prev = null;
+            return inorder(root);
+        }
+
+        private boolean inorder(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            if (!inorder(root.left)) {
+                return false;
+            }
+
+            if (prev != null && root.val <= prev.val) {
+                return false;
+            }
+
+            prev = root;
+
+            return inorder(root.right);
+        }
+    }
+
+    class Sol_inorder_iterative {
+        public boolean isValidBST_iterative(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            Stack<TreeNode> s = new Stack<>();
+            TreeNode prev = null;
+            TreeNode cur = root;
+
+            while (!s.isEmpty() || cur != null) {
+                while (cur != null) {
+                    s.push(cur);
+                    cur = cur.left;
+                }
+
+                TreeNode node = s.pop();
+                if (prev != null && node.val <= prev.val) {
+                    return false;
+                }
+
+                prev = node;
+                cur = node.right;
+            }
+
+            return true;
+        }
+    }
 }
