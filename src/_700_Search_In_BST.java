7c6ce54829ad468d880df6484d439652fcdb8644
LiFangCoding <fanglihust@gmail.com>
2020-03-02 19:26:27 +0900

    refactor 700

--- src/_700_Search_In_BST.java
+++ src/_700_Search_In_BST.java
@@ -44,17 +44,11 @@ public class _700_Search_In_BST {
 
     class Sol_recursion {
         public TreeNode searchBST(TreeNode root, int val) {
-            if (root == null) {
-                return null;
+            if (root == null || root.val == val) {
+                return root;
             }
 
-            if (root.val == val) {
-                return root;
-            } else if (root.val < val) {
-                return searchBST(root.right, val);
-            } else {
-                return searchBST(root.left, val);
-            }
+            return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
         }
     }
 }
