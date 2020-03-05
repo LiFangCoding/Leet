d7a03bc14c496a2ac1e9b4831dc15531ebdc15d8
LiFangCoding <fanglihust@gmail.com>
2020-03-05 15:09:45 +0900

    refactor 105

--- src/_101_150/_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ src/_101_150/_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -25,52 +25,51 @@ import java.util.Map;
  * 15   7
  */
 public class _105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
-    Map<Integer, Integer> map;
+  // preorder:  root, l, r. we can get part from len
+  // inorder: l, root, r
+  // val -> idx
+  Map<Integer, Integer> map;
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || inorder == null) {
-            return null;
-        }
-        int len1 = preorder.length;
-        int len2 = inorder.length;
-        if (len1 != len2) {
-            return null;
-        }
-
-        map = new HashMap<>();
-        for (int i = 0; i < len1; i++) {
-            map.put(inorder[i], i);
-        }
-
-        /**
-         * !!! is len1 - 1, not len1
-         */
-        return helper(preorder, 0, len1 - 1, inorder, 0, len1 - 1);
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    if (preorder == null || inorder == null) {
+      return null;
     }
 
-    private TreeNode helper(int[] preorder, int start1, int end1, int[] inorder, int start2, int end2) {
-        if (start1 == end1) {
-            return new TreeNode(preorder[start1]);
-        }
-
-        if (start1 > end1) {
-            return null;
-        }
-
-        TreeNode root = new TreeNode(preorder[start1]);
-        int rootIndex = map.get(preorder[start1]);
-
-        int leftSubLen = rootIndex - start2;
-        /**
-         * root, leftsub, rightsub
-         * leftsub, root, rightsub
-         */
-        root.left = helper(preorder, start1 + 1, start1 + leftSubLen,
-                inorder, start2, rootIndex - 1);
-
-        root.right = helper(preorder, start1 + leftSubLen + 1, end1,
-                inorder, rootIndex + 1, end2);
-
-        return root;
+    int len1 = preorder.length;
+    int len2 = inorder.length;
+    if (len1 != len2) {
+      return null;
     }
+
+    map = new HashMap<>();
+    for (int i = 0; i < inorder.length; i++) {
+      map.put(inorder[i], i);
+    }
+
+    return helper(preorder, 0, len1 - 1, inorder, 0, len2 - 1);
+  }
+
+  private TreeNode helper(int[] preorder, int l1, int r1, int[] inorder, int l2, int r2) {
+    if (l1 > r1) {
+      return null;
+    }
+
+    if (l1 == r1) {
+      return new TreeNode(preorder[l1]);
+    }
+
+    TreeNode root = new TreeNode(preorder[l1]);
+    int idx = map.get(preorder[l1]);
+
+    /**
+     * root, leftsub, rightsub
+     * leftsub, root, rightsub
+     * !!! Abstract, do not think l1 = 0, l2 = 0.
+     */
+    int addLeftSize = idx - 1 - l2;
+    root.left = helper(preorder, l1 + 1, l1 + 1 + addLeftSize, inorder, l2, idx - 1);
+    root.right = helper(preorder, l1 + 2 + addLeftSize, r1, inorder, idx + 1, r2);
+
+    return root;
+  }
 }
