28406c23b2a919011dfa72598aa5415350d457cd
LiFangCoding <fanglihust@gmail.com>
2020-04-01 17:34:03 +0900

    finish 301

--- src/_301_350/_301_Remove_Invalid_Parentheses.java
+++ src/_301_350/_301_Remove_Invalid_Parentheses.java
@@ -1,5 +1,6 @@
 package _301_350;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,7 +22,72 @@ import java.util.List;
  * Output: [""]
  */
 public class _301_Remove_Invalid_Parentheses {
+    List<String> ans;
+
     public List<String> removeInvalidParentheses(String s) {
-        return null;
+        // 寻找不合法半边括号的个数
+        int l = 0;
+        int r = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                l++;
+            } else if (c == ')') {
+                if (l > 0) {
+                    l--;
+                } else {
+                    r++;
+                }
+            }
+        }
+
+        ans = new ArrayList<>();
+
+        dfs(s, 0, l, r);
+        return ans;
+    }
+
+    private boolean isValid(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                ++count;
+            } else if (c == ')') {
+                --count;
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+        return count == 0;
+    }
+
+
+    // l/r: number of left/right parentheses to remove.
+    void dfs(String s, int st, int l, int r) {
+        // Nothing to remove.
+        if (l == 0 && r == 0) {
+            if (isValid(s)) {
+                ans.add(s);
+            }
+            return;
+        }
+
+        for (int i = st; i < s.length(); ++i) {
+            // 去重
+            if (i != st && s.charAt(i) == s.charAt(i - 1)) {
+                continue;
+            }
+
+            String change = s.substring(0, i) + s.substring(i + 1);
+
+            if (l > 0 && s.charAt(i) == '(') {
+                dfs(change, i, l - 1, r);
+            }
+
+            if (r > 0 && s.charAt(i) == ')') {
+                dfs(change, i, l, r - 1);
+            }
+        }
     }
 }
