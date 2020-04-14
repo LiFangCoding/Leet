d96ff93b48285f9f7a95c9e3b3b3eefaffc08a85
LiFangCoding <fanglihust@gmail.com>
2020-04-15 01:09:30 +0900

    add a try for 149

--- src/_101_150/_149_Max_Points_on_a_Line.java
+++ src/_101_150/_149_Max_Points_on_a_Line.java
@@ -33,6 +33,58 @@ import java.util.Map;
  * 0  1  2  3  4  5  6
  */
 public class _149_Max_Points_on_a_Line {
+    public static void main(String[] args) {
+        int[][] points = {{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}};
+        _149_Max_Points_on_a_Line test = new _149_Max_Points_on_a_Line();
+        System.out.println(test.maxPoints_my(points));
+    }
+
+    public int maxPoints_my(int[][] points) {
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+
+        int n = points.length;
+        int ans = 1;
+
+        // can have [[0,0],[1,1],[0,0]]
+        for (int i = 0; i < n; i++) {
+            // slope -> cnt
+            Map<Double, Integer> map = new HashMap<>();
+            // careful include itself
+            int vertCnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (points[j][0] == points[i][0]) {
+                    vertCnt++;
+                    System.out.println("verCnt is " + vertCnt);
+                    ans = Math.max(ans, vertCnt);
+                    System.out.println("ans is " + ans);
+
+                } else {
+                    double slope = getSlope(points[i], points[j]);
+                    System.out.println("Points are " + points[i][0] + "," + points[i][1] + " "
+                            + points[j][0] + "," + points[j][1] + " slope is " + slope);
+                    map.put(slope, map.getOrDefault(slope, 1) + 1);
+                    System.out.println("map count is " + map.get(slope));
+                    ans = Math.max(ans, map.get(slope));
+                    System.out.println("ans is " + ans);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private double getSlope(int[] p1, int[] p2) {
+        // System.out.println("p1 x is " + p1[0] + " y is " + p1[1]);
+        // System.out.println("p2 x is " + p2[0] + " y is " + p2[1]);
+
+        return (double) (p2[1] - p1[1]) / (p2[0] - p1[0]);
+    }
+
     //TODO: need more practice
     public int maxPoints(int[][] points) {
         // Write your code here
@@ -43,7 +95,7 @@ public class _149_Max_Points_on_a_Line {
 
         for (int i = 0; i < points.length; i++) {
             Map<String, Integer> slope = new HashMap<>();
-
+            // because can have the overlap points. [[1,1],[1,1],[2,2],[2,2]] ans = 4
             int maxPoints = 0, overlap = 0, vertical = 0;
 
             for (int j = i + 1; j < points.length; j++) {
