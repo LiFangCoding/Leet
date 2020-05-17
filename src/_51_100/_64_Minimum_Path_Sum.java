95221dca23d1a2e1d1f5f66fb37e7f123fd2bea7
LiFangCoding <fanglihust@gmail.com>
2020-05-18 01:30:17 +0900

    refactor 64

--- src/_51_100/_64_Minimum_Path_Sum.java
+++ src/_51_100/_64_Minimum_Path_Sum.java
@@ -22,27 +22,27 @@ public class _64_Minimum_Path_Sum {
             return 0;
         }
 
-        int m = grid.length;
-        int n = grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-        int[][] f = new int[m][n];
+        int[][] f = new int[rows][cols];
 
         f[0][0] = grid[0][0];
 
-        for (int i = 1; i < m; i++) {
+        for (int i = 1; i < rows; i++) {
             f[i][0] = f[i - 1][0] + grid[i][0];
         }
 
-        for (int j = 1; j < n; j++) {
+        for (int j = 1; j < cols; j++) {
             f[0][j] = f[0][j - 1] + grid[0][j];
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
                 f[i][j] = Math.min(f[i - 1][j], f[i][j - 1]) + grid[i][j];
             }
         }
 
-        return f[m - 1][n - 1];
+        return f[rows - 1][cols - 1];
     }
 }
