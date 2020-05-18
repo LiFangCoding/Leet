a56744204416881bce8f251499fd78aeddf2aec2
LiFangCoding <fanglihust@gmail.com>
2020-05-18 14:28:09 +0900

    refactor 93

--- src/_51_100/_93_Restore_IP_Addresses.java
+++ src/_51_100/_93_Restore_IP_Addresses.java
@@ -12,57 +12,60 @@ import java.util.List;
  * Output: ["255.255.11.135", "255.255.111.35"]
  */
 public class _93_Restore_IP_Addresses {
-    public static void main(String[] args) {
-        _93_Restore_IP_Addresses test = new _93_Restore_IP_Addresses();
-        System.out.println(test.restoreIpAddresses("25525511135"));
+  public static void main(String[] args) {
+    _93_Restore_IP_Addresses test = new _93_Restore_IP_Addresses();
+    System.out.println(test.restoreIpAddresses("25525511135"));
 
-        System.out.println(Integer.parseInt("01"));
+    System.out.println(Integer.parseInt("01"));
+  }
+
+  List<String> ans;
+  public List<String> restoreIpAddresses(String s) {
+    ans = new ArrayList<>();
+
+    int len = s.length();
+    if (len < 4 || len > 12) {
+      return ans;
     }
 
-    public List<String> restoreIpAddresses(String s) {
-        List<String> res = new ArrayList<>();
-        if (s == null || s.length() == 0) {
-            return res;
-        }
+    dfs(s, 0, "", 0);
+    return ans;
+  }
 
-        int len = s.length();
-        if (len < 4 || len > 12) {
-            return res;
-        }
 
-        helper(s, 0, 0, "", res);
-        return res;
+  // "010010"
+  // ["0.1.0.10","0.1.0.10","0.1.1.0","0.10.0.10","0.10.1.0","0.100.1.0","1.0.0.10","1.0.1.0","1.0.1.0","10.0.1.0"]
+  // exp : ["0.10.0.10","0.100.1.0"]
+  private void dfs(String s, int start, String path, int cnt) {
+    if (start == s.length()) {
+      if (cnt == 4) {
+        ans.add(path.substring(0, path.length() - 1));
+      }
+
+      return;
     }
 
-    private void helper(String s, int start, int count, String chosen, List<String> res) {
-        if (count == 4 && start == s.length()) {
-            res.add(chosen.substring(0, chosen.length() - 1));
-            return;
-        }
-
-        /**
-         * Important prune. Increase from 10% to 90 %
-         */
-        if (count > 4) {
-            return;
-        }
-
-        for (int i = 1; i <= 3 && start + i <= s.length(); i++) {
-            String str = s.substring(start, start + i);
-            /**
-             * find valid chosen
-             * 01 is not correct
-             */
-            if (str.charAt(0) == '0' && str.length() > 1) {
-                continue;
-            }
-            int num = Integer.parseInt(str);
-            /**
-             * !! only 0 is ok
-             */
-            if (num >= 0 && num <= 255) {
-                helper(s, start + i, count + 1, chosen + num + '.', res);
-            }
-        }
+    /**
+     * Important prune. Increase from 10% to 90 %
+     */
+    if (cnt > 4) {
+      return;
     }
+
+    for (int i = start; i < start + 4 && i < s.length(); i++) {
+      String subS = s.substring(start, i + 1);
+
+      // important to skip the s which start with 0
+      //  01 is not correct
+      if (subS.length() > 1 && subS.charAt(0) == '0') {
+        continue;
+      }
+
+      int val = Integer.parseInt(subS);
+
+      if (val >= 0 && val <= 255) {
+        dfs(s, i + 1, path + val + ".", cnt + 1);
+      }
+    }
+  }
 }
