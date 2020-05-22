f51c67e29d3e14bf79b8f0a4f3721c5487843af3
LiFangCoding <fanglihust@gmail.com>
2020-05-22 18:47:01 +0900

    refactor 105

--- src/_101_150/_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ src/_101_150/_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -2,7 +2,6 @@ package _101_150;
 
 import common.TreeNode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -50,25 +49,24 @@ public class _105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
   }
 
   private TreeNode helper(int[] preorder, int l1, int r1, int[] inorder, int l2, int r2) {
+    // cannot ignore. Else index 0 out of bound 0. [], []
     if (l1 > r1) {
       return null;
     }
 
-    if (l1 == r1) {
-      return new TreeNode(preorder[l1]);
-    }
+    // this can ignore and correct val.
+//    if (l1 == r1) {
+//      return new TreeNode(preorder[l1]);
+//    }
 
-    TreeNode root = new TreeNode(preorder[l1]);
-    int idx = map.get(preorder[l1]);
+    int rootVal = preorder[l1];
+    int rootIdx2 = map.get(preorder[l1]);
 
-    /**
-     * root, leftsub, rightsub
-     * leftsub, root, rightsub
-     * !!! Abstract, do not think l1 = 0, l2 = 0.
-     */
-    int addLeftSize = idx - 1 - l2;
-    root.left = helper(preorder, l1 + 1, l1 + 1 + addLeftSize, inorder, l2, idx - 1);
-    root.right = helper(preorder, l1 + 2 + addLeftSize, r1, inorder, idx + 1, r2);
+    int leftLen = rootIdx2 - l2;
+
+    TreeNode root = new TreeNode(rootVal);
+    root.left = helper(preorder, l1 + 1, l1 + leftLen, inorder, l2, rootIdx2 - 1);
+    root.right = helper(preorder, l1 + leftLen + 1, r1, inorder, rootIdx2 + 1, r2);
 
     return root;
   }
