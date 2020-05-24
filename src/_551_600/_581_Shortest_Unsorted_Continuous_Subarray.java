4c6547319f2a5aebb98605df2ef8e280beae1f5b
LiFangCoding <fanglihust@gmail.com>
2020-05-25 01:11:34 +0900

    finish 343

---
+++ src/_551_600/_581_Shortest_Unsorted_Continuous_Subarray.java
@@ -0,0 +1,36 @@
+package _551_600;
+
+/**
+ * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+ * <p>
+ * You need to find the shortest such subarray and output its length.
+ * <p>
+ * Example 1:
+ * Input: [2, 6, 4, 8, 10, 9, 15]
+ * Output: 5
+ * Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+ * Note:
+ * Then length of the input array is in range [1, 10,000].
+ * The input array may contain duplicates, so ascending order here means <=.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _581_Shortest_Unsorted_Continuous_Subarray {
+    /**
+     * T = nlogn
+     */
+    class Sol_Sort {
+        public int findUnsortedSubarray(int[] A) {
+            int len = A.length;
+            int[] sorted = new int[A.length];
+
+            for (int i = 0; i < len; i++) {
+                sorted[i] = A[i];
+            }
+
+
+        }
+    }
+}
