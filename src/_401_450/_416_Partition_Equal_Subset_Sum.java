0c8f63607f2e6c9890eed1e8685029f892a7717d
LiFangCoding <fanglihust@gmail.com>
2020-05-25 17:14:34 +0900

    finish 72

--- src/_401_450/_416_Partition_Equal_Subset_Sum.java
+++ src/_401_450/_416_Partition_Equal_Subset_Sum.java
@@ -34,16 +34,54 @@ import java.util.Map;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class _416_Partition_Equal_Subset_Sum {
-    //TODO: add dp sol
+  /**
+   *
+   */
+  class Sol_dp {
+    public boolean canPartition(int[] nums) {
+      int sum = 0;
+      for (int num : nums) {
+        sum += num;
+      }
 
-    /**
-     * 34 ms
-     * <p>
-     * Search with memo
-     * If without memo, 超出时间限制
-     */
-    class Sol_Memo {
-        Map<String, Boolean> map;
+      if (sum % 2 == 1) {
+        return false;
+      }
+
+      int target = sum / 2;
+      int n = nums.length;
+      // f[i][w] first i to get weight w
+      // dp[i][j] = x 表示，对于前 i 个物品，当前背包的容量为 j 时，若 x 为 true，则说明可以恰好将背包装满，若 x 为 false，则说明不能恰好将背包装满。
+      boolean[][] f = new boolean[n + 1][target + 1];
+
+      for (int i = 0; i <= n; i++) {
+        f[i][0] = true;
+      }
+
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= target; j++) {
+          f[i][j] = f[i - 1][j];
+
+          int choice = nums[i - 1];
+
+          if (j - choice >= 0) {
+            f[i][j] = f[i][j] || f[i - 1][j - choice];
+          }
+        }
+      }
+
+      return f[n][target];
+    }
+  }
+
+  /**
+   * 34 ms
+   * <p>
+   * Search with memo
+   * If without memo, 超出时间限制
+   */
+  class Sol_Memo {
+    Map<String, Boolean> map;
 
         public boolean canPartition(int[] A) {
             if (A == null || A.length == 0) {
--- src/_51_100/_72_Edit_Distance.java
+++ src/_51_100/_72_Edit_Distance.java
@@ -1,48 +1,94 @@
 package _51_100;
 
 public class _72_Edit_Distance {
-    public static void main(String[] args) {
-        _72_Edit_Distance test = new _72_Edit_Distance();
-        System.out.println("exp is 3, true is " + test.minDistance("horse", "ros"));
+
+  /**
+   * 7ms
+   * T = O(mn)，其中 mm 为 word1 的长度，nn 为 word2 的长度。
+   * S = O(mn)，我们需要大小为 O(mn)O(mn) 的 DD 数组来记录状态值。
+   * For two strings, think dp table i and j to solve
+   */
+  class Sol_DP_loop {
+    public int minDistance(String s1, String s2) {
+      /**
+       * find the minimum number of operations required to convert word1 to word2.
+       *
+       * the first mth character of s1
+       * the first nth character of s2
+       * f[m][n] = f[m - 1][n] + 1; deletion
+       *           f[m][n - 1] + 1; insertion
+       *           f[m - 1][n - 1] + { s1.charAt(m) == s2.charAt(n) ? 0 : 1}
+       *
+       * res is f[len1][len2]
+       */
+      int len1 = s1.length();
+      int len2 = s2.length();
+
+      /**
+       * f is 0,1,... len1
+       *      0,1,.. len2
+       */
+      int[][] f = new int[len1 + 1][len2 + 1];
+
+      for (int i = 0; i <= len1; i++) {
+        f[i][0] = i;
+      }
+
+      for (int i = 0; i <= len2; i++) {
+        f[0][i] = i;
+      }
+
+      for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+          f[i][j] = f[i - 1][j] + 1;
+          f[i][j] = Math.min(f[i][j], f[i][j - 1] + 1);
+          f[i][j] = Math.min(f[i][j], f[i - 1][j - 1] + (s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1));
+        }
+      }
+
+      return f[len1][len2];
     }
+  }
+
+  /**
+   * 4ms
+   * T = O(mn)，其中 mm 为 word1 的长度，nn 为 word2 的长度。
+   * S = O(mn)，我们需要大小为 O(mn)O(mn) 的 DD 数组来记录状态值。
+   */
+  class Sol_DP_recursion {
+    Integer[][] memo;
 
     public int minDistance(String s1, String s2) {
-        /**
-         * find the minimum number of operations required to convert word1 to word2.
-         *
-         * the first mth character of s1
-         * the first nth character of s2
-         * f[m][n] = f[m - 1][n] + 1; deletion
-         *           f[m][n - 1] + 1; insertion
-         *           f[m - 1][n - 1] + { s1.charAt(m) == s2.charAt(n) ? 0 : 1}
-         *
-         * res is f[len1][len2]
-         */
-        int len1 = s1.length();
-        int len2 = s2.length();
+      memo = new Integer[s1.length()][s2.length()];
 
-        /**
-         * f is 0,1,... len1
-         *      0,1,.. len2
-         */
-        int[][] f = new int[len1 + 1][len2 + 1];
-
-        for (int i = 0; i <= len1; i++) {
-            f[i][0] = i;
-        }
-
-        for (int i = 0; i <= len2; i++) {
-            f[0][i] = i;
-        }
-
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
-                f[i][j] = f[i - 1][j] + 1;
-                f[i][j] = Math.min(f[i][j], f[i][j - 1] + 1);
-                f[i][j] = Math.min(f[i][j], f[i - 1][j - 1] + (s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1));
-            }
-        }
-
-        return f[len1][len2];
+      // from 0,...ith char.
+      return min(s1, s1.length() - 1, s2, s2.length() - 1);
     }
+
+    private int min(String s1, int i1, String s2, int i2) {
+      if (i1 == -1) {
+        return i2 + 1;
+      }
+
+      if (i2 == -1) {
+        return i1 + 1;
+      }
+
+      if (memo[i1][i2] != null) {
+        return memo[i1][i2];
+      }
+
+      if (s1.charAt(i1) == s2.charAt(i2)) {
+        memo[i1][i2] = min(s1, i1 - 1, s2, i2 - 1);
+      } else {
+        int insert = min(s1, i1, s2, i2 - 1) + 1;
+        int delete = min(s1, i1 - 1, s2, i2) + 1;
+        int replace = min(s1, i1 - 1, s2, i2 - 1) + 1;
+
+        memo[i1][i2] = Math.min(insert, Math.min(delete, replace));
+      }
+
+      return memo[i1][i2];
+    }
+  }
 }
