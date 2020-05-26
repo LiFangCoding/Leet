5f50d93cc3bd210e8e8acfbd395d613150925bd1
LiFangCoding <fanglihust@gmail.com>
2020-05-26 17:28:48 +0900

    add todo 399

--- src/_351_400/_399_Evaluate_Division.java
+++ src/_351_400/_399_Evaluate_Division.java
@@ -2,10 +2,8 @@ package _351_400;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -32,93 +30,123 @@ import java.util.Set;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class _399_Evaluate_Division {
-  //TODO: https://www.acwing.com/solution/content/10234/
-  class Edge {
-    String y;
-    boolean isMul;
-    double rank;
-
-    public Edge(String y, double rank, boolean isMul) {
-      this.y = y;
-      this.rank = rank;
-      this.isMul = isMul;
-    }
-  }
-
-  Map<String, Set<Edge>> build(List<List<String>> equations, double[] values) {
-    int n = equations.size();
-    Map<String, Set<Edge>> graph = new HashMap<>();
-    for (int i = 0; i < n; i++) {
-      List<String> equation = equations.get(i);
-      String x = equation.get(0);
-      String y = equation.get(1);
-      double rank = values[i];
-
-      if (graph.get(x) == null)
-        graph.put(x, new HashSet<>());
-      graph.get(x).add(new Edge(y, rank, true));
-
-      if (graph.get(y) == null)
-        graph.put(y, new HashSet<>());
-      graph.get(y).add(new Edge(x, rank, false));
-    }
-    return graph;
-  }
+  // TODO : https://zxi.mytechroad.com/blog/graph/leetcode-399-evaluate-division/
+  // https://www.acwing.com/solution/content/10234/
 
   /**
-   * BFS:
    * 1ms
-   * T = ?
-   * S = ?
-   * <p>
-   * DFS
    */
-  class Sol_BFS {
+  public class Sol_BFS {
+    Map<String, HashMap<String, Double>> g = new HashMap<>();
+
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-      Map<String, Set<Edge>> graph = build(equations, values);
-      int n = queries.size();
-      double[] result = new double[n];
-      for (int i = 0; i < n; i++) {
-        result[i] = suggest(queries.get(i), graph);
+      for (int i = 0; i < equations.size(); ++i) {
+        String x = equations.get(i).get(0);
+        String y = equations.get(i).get(1);
+        double k = values[i];
+        g.computeIfAbsent(x, l -> new HashMap<String, Double>()).put(y, k);
+        g.computeIfAbsent(y, l -> new HashMap<String, Double>()).put(x, 1.0 / k);
       }
-      return result;
-    }
 
-    double suggest(List<String> query, Map<String, Set<Edge>> graph) {
-      String x = query.get(0);
-      String y = query.get(1);
-      if (graph.get(x) == null || graph.get(y) == null)
-        return -1.0;
-      if (x.equals(y))
-        return 1.0;
-      return bfs(x, y, graph);
+      double[] ans = new double[queries.size()];
 
-      // double[] res = {1.0, 1.0};
-      // boolean r = dfs(x, y, res, graph);
-      // graph.get(x).add(new Edge(y, res[0], true));
-      // graph.get(y).add(new Edge(x, res[0], false));
-      // return r ? res[0]/res[1] : -1.0;
-    }
-
-    double bfs(String x, String y, Map<String, Set<Edge>> graph) {
-      Queue<String> queue = new LinkedList<>();
-      Map<String, Double> values = new HashMap<>();
-      queue.offer(x);
-      values.put(x, 1.0);
-
-      while (!queue.isEmpty()) {
-        String top = queue.poll();
-        double value = values.get(top);
-        Set<Edge> edges = graph.get(top);
-        for (Edge e : edges) {
-          if (values.get(e.y) != null)
-            continue;
-          values.put(e.y, e.isMul ? value * e.rank : value / e.rank);
-          queue.offer(e.y);
+      for (int i = 0; i < queries.size(); ++i) {
+        String x = queries.get(i).get(0);
+        String y = queries.get(i).get(1);
+        if (!g.containsKey(x) || !g.containsKey(y)) {
+          ans[i] = -1.0;
+        } else {
+          ans[i] = divide(x, y, new HashSet<String>());
         }
       }
-      Double res = values.get(y);
-      return res == null ? -1.0 : res;
+
+      return ans;
+    }
+
+    private double divide(String x, String y, Set<String> visited) {
+      if (x.equals(y))
+        return 1.0;
+      visited.add(x);
+      if (!g.containsKey(x))
+        return -1.0;
+      for (String n : g.get(x).keySet()) {
+        if (visited.contains(n))
+          continue;
+        visited.add(n);
+        double d = divide(n, y, visited);
+        if (d > 0)
+          return d * g.get(x).get(n);
+      }
+      return -1.0;
+    }
+  }
+
+  public class Sol_UF {
+    class Node {
+      public String parent;
+      public double ratio;
+
+      public Node(String parent, double ratio) {
+        this.parent = parent;
+        this.ratio = ratio;
+      }
+    }
+
+    /**
+     * 3ms
+     */
+    class UnionFindSet {
+      private Map<String, Node> parents = new HashMap<>();
+
+      public Node find(String s) {
+        if (!parents.containsKey(s))
+          return null;
+        Node n = parents.get(s);
+        if (!n.parent.equals(s)) {
+          Node p = find(n.parent);
+          n.parent = p.parent;
+          n.ratio *= p.ratio;
+        }
+        return n;
+      }
+
+      public void union(String s, String p, double ratio) {
+        boolean hasS = parents.containsKey(s);
+        boolean hasP = parents.containsKey(p);
+        if (!hasS && !hasP) {
+          parents.put(s, new Node(p, ratio));
+          parents.put(p, new Node(p, 1.0));
+        } else if (!hasP) {
+          parents.put(p, new Node(s, 1.0 / ratio));
+        } else if (!hasS) {
+          parents.put(s, new Node(p, ratio));
+        } else {
+          Node rS = find(s);
+          Node rP = find(p);
+          rS.parent = rP.parent;
+          rS.ratio = ratio / rS.ratio * rP.ratio;
+        }
+      }
+    }
+
+    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+      UnionFindSet u = new UnionFindSet();
+
+      for (int i = 0; i < equations.length; ++i)
+        u.union(equations[i][0], equations[i][1], values[i]);
+
+      double[] ans = new double[queries.length];
+
+      for (int i = 0; i < queries.length; ++i) {
+        Node rx = u.find(queries[i][0]);
+        Node ry = u.find(queries[i][1]);
+        if (rx == null || ry == null || !rx.parent.equals(ry.parent))
+          ans[i] = -1.0;
+        else
+          ans[i] = rx.ratio / ry.ratio;
+      }
+
+      return ans;
     }
   }
 }
