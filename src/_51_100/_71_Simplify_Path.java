66b7ef84604fa26e5896936ee2262f014909a91b
LiFangCoding <fanglihust@gmail.com>
2020-06-02 23:31:01 +0900

    add sol 71

--- src/_51_100/_71_Simplify_Path.java
+++ src/_51_100/_71_Simplify_Path.java
@@ -1,55 +1,101 @@
 package _51_100;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.*;
 
 public class _71_Simplify_Path {
-    public static void main(String[] args) {
-        String s = "/usr/stories//hello/../work///for";
-        _71_Simplify_Path test = new _71_Simplify_Path();
-        System.out.println(Arrays.toString(s.split("/+")));
-        System.out.println(test.simplifyPath(s));
-    }
 
-    public String simplifyPath(String path) {
-        if (path == null || path.length() == 0) {
-            return "/";
-        }
-
-        Deque<String> deque = new ArrayDeque<>();
-        String[] dirs = path.split("/+");
-
-        /**
-         * !!! dir is "". But dir == "" is false. We need dir.equals().
-         */
-        for (String dir : dirs) {
-            if (dir.equals("") || dir.equals(".")) {
-                continue;
+    class Sol_Deque {
+        public String simplifyPath(String path) {
+            if (path == null || path.length() == 0) {
+                return "/";
             }
 
-            if (dir.equals("..")) {
-                if (!deque.isEmpty()) {
-                    deque.removeFirst();
+            Deque<String> deque = new ArrayDeque<>();
+            String[] dirs = path.split("/+");
+
+            /**
+             * !!! dir is "". But dir == "" is false. We need dir.equals().
+             */
+            for (String dir : dirs) {
+                if (dir.equals("") || dir.equals(".")) {
+                    continue;
                 }
-            } else {
-                deque.addFirst(dir);
+
+                if (dir.equals("..")) {
+                    if (!deque.isEmpty()) {
+                        deque.removeFirst();
+                    }
+                } else {
+                    deque.addFirst(dir);
+                }
             }
-        }
 
-        if (deque.isEmpty()) {
-            return "/";
-        }
-        /**
-         * !!! careful. If deque is empty, it should be "/".
-         */
-        StringBuilder sb = new StringBuilder();
-        while (!deque.isEmpty()) {
-            sb.append("/");
-            sb.append(deque.removeLast());
-        }
+            if (deque.isEmpty()) {
+                return "/";
+            }
+            /**
+             * !!! careful. If deque is empty, it should be "/".
+             */
+            StringBuilder sb = new StringBuilder();
+            while (!deque.isEmpty()) {
+                sb.append("/");
+                sb.append(deque.removeLast());
+            }
 
-        return sb.toString();
+            return sb.toString();
+        }
     }
+
+    /**
+     * 15ms
+     * T = n
+     */
+    class Sol_Stack {
+        public String simplifyPath(String path) {
+            if (path == null || path.length() == 0) {
+                return "/";
+            }
+
+            String[] strs = path.split("/+");
+            // we need to remove from the last
+            // a b c   /a/b/c   c/b/a/
+            Stack<String> stack = new Stack<>();
+
+            for (String s : strs) {
+                if (s.equals("") || s.equals(".")) {
+                    continue;
+                }
+
+                if (s.equals("..")) {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                } else {
+                    stack.push(s);
+                }
+            }
+
+            List<String> list = new ArrayList<>();
+            while (!stack.isEmpty()) {
+                list.add(stack.pop());
+            }
+            Collections.reverse(list);
+            return "/" + String.join("/", list);
+//        StringBuilder sb = new StringBuilder();
+//
+//        while (!stack.isEmpty()) {
+//            sb.append(stack.pop());
+//            sb.append("/");
+//        }
+            // important. cannot use sb.reverse() "/home/" actual: "/emoh"
+//        sb.reverse();
+//        if (sb.length() == 0) {
+//            return "/";
+//        }
+//
+//        return sb.toString();
+        }
+    }
+
 
 }
