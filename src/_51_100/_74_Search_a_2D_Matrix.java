4d86429e2f612b963fa27b689b1b78d147465da4
LiFangCoding <fanglihust@gmail.com>
2020-06-03 10:51:59 +0900

    refactor 74

--- src/_51_100/_74_Search_a_2D_Matrix.java
+++ src/_51_100/_74_Search_a_2D_Matrix.java
@@ -1,49 +1,38 @@
 package _51_100;
 
 public class _74_Search_a_2D_Matrix {
-    /**
-     * matrix
-     * 0, 1, 2
-     * 3, 4, 5
-     * m = 2
-     * n = 3
-     *
-     * @param M
-     * @param target
-     * @return
-     */
-    public boolean searchMatrix(int[][] M, int target) {
-        if (M == null || M.length == 0 || M[0].length == 0) {
-            return false;
-        }
-
-        int m = M.length;
-        int n = M[0].length;
-
-        int start = 0;
-        int end = m * n - 1;
-
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            /**
-             * !!! here is n. Not m.
-             */
-            int row = mid / n;
-            int col = mid % n;
-
-            if (M[row][col] == target) {
-                return true;
-            } else if (M[row][col] < target) {
-                start = mid;
-            } else {
-                end = mid;
-            }
-        }
-
-        if (M[end / n][end % n] == target) {
-            return true;
-        }
-
-        return M[start / n][start % n] == target;
+  /**
+   * matrix
+   * 0, 1, 2
+   * 3, 4, 5
+   * m = 2
+   * n = 3
+   */
+  public boolean searchMatrix(int[][] matrix, int target) {
+    if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+      return false;
     }
+
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+
+    int n = rows * cols;
+
+    int l = 0, r = n - 1;
+
+    while (l + 1 < r) {
+      int mid = l + (r - l) / 2;
+      int row = mid / cols;
+      int col = mid % cols;
+      if (matrix[row][col] == target) {
+        return true;
+      } else if (matrix[row][col] < target) {
+        l = mid;
+      } else {
+        r = mid;
+      }
+    }
+
+    return matrix[l / cols][l % cols] == target || matrix[r / cols][r % cols] == target;
+  }
 }
