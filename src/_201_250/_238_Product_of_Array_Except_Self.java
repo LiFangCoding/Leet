751483423b0c4f2812115f1150c92189e38d5937
LiFangCoding <fanglihust@gmail.com>
2020-06-04 10:03:45 +0900

    refactor 238

--- src/_201_250/_238_Product_of_Array_Except_Self.java
+++ src/_201_250/_238_Product_of_Array_Except_Self.java
@@ -13,7 +13,47 @@ package _201_250;
  * Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
  */
 public class _238_Product_of_Array_Except_Self {
+  /**
+   * 1ms
+   * T = n
+   * S = n
+   */
+  class Sol_use_array {
+    public int[] productExceptSelf(int[] A) {
+      if (A == null || A.length == 0) {
+        return new int[0];
+      }
+
+      int len = A.length;
+      // f[i] means product of [0,i)
+      int[] f = new int[A.length];
+      f[0] = 1;
+      // g[i] means product of (i, len - 1]
+      int[] g = new int[A.length];
+      g[len - 1] = 1;
+
+      for (int i = 1; i < len; i++) {
+        f[i] = f[i - 1] * A[i - 1];
+      }
+
+      for (int i = len - 2; i >= 0; i--) {
+        g[i] = g[i + 1] * A[i + 1];
+      }
+
+      int[] ans = new int[len];
+      for (int i = 0; i < len; i++) {
+        ans[i] = f[i] * g[i];
+      }
+
+      return ans;
+    }
+  }
+
+  class Sol_in_place {
     /**
+     * 1ms
+     * T = n
+     * S = 1
      * [1,2,3,4]
      * [24,12,8,6]
      * <p>
@@ -32,24 +72,25 @@ public class _238_Product_of_Array_Except_Self {
      * [20 ,15,12]
      */
     public int[] productExceptSelf(int[] A) {
-        if (A == null || A.length == 0) {
-            return new int[0];
-        }
+      if (A == null || A.length == 0) {
+        return new int[0];
+      }
 
-        int len = A.length;
-        int[] res = new int[len];
+      int len = A.length;
+      int[] ans = new int[len];
 
-        res[0] = 1;
-        for (int i = 1; i < len; i++) {
-            res[i] = res[i - 1] * A[i - 1];
-        }
+      ans[0] = 1;
+      for (int i = 1; i < len; i++) {
+        ans[i] = ans[i - 1] * A[i - 1];
+      }
 
-        int postSum = 1;
-        for (int j = len - 1; j >= 0; j--) {
-            res[j] = res[j] * postSum;
-            postSum *= A[j];
-        }
+      int postSum = 1;
+      for (int j = len - 1; j >= 0; j--) {
+        ans[j] = ans[j] * postSum;
+        postSum *= A[j];
+      }
 
-        return res;
+      return ans;
     }
+  }
 }
