beb98beea56453011d15c7c7fc0bac2c9999b480
LiFangCoding <fanglihust@gmail.com>
2020-06-06 02:20:56 +0900

    add sol 233

--- src/_201_250/_233_NumberofDigitOne.java
+++ src/_201_250/_233_NumberofDigitOne.java
@@ -10,7 +10,28 @@ package _201_250;
  * Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
  */
 public class _233_NumberofDigitOne {
-    public int countDigitOne(int n) {
-        return -1;
+  //TODO:
+
+  /**
+   * https://www.acwing.com/solution/content/280/
+   * https://www.acwing.com/solution/content/280/
+   *
+   * @param n
+   * @return
+   */
+  public int countDigitOne(int n) {
+    if (n <= 0)
+      return 0;
+    if (n < 10)
+      return 1;
+    String s = String.valueOf(n);
+    int last = Integer.parseInt(s.substring(1));
+    int power = (int) Math.pow(10, s.length() - 1);
+    int high = s.charAt(0) - '0';//当前字符的ASCII码减去‘0’的ASCII码
+    if (high == 1) {
+      return countDigitOne(last) + countDigitOne(power - 1) + last + 1;
+    } else {
+      return power + high * countDigitOne(power - 1) + countDigitOne(last);
     }
+  }
 }
