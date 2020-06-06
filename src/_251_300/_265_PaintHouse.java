d52066d6f83b6eb2a59c32aeee7402c1b6f2f11c
LiFangCoding <fanglihust@gmail.com>
2020-06-06 14:58:02 +0900

    add sol 265

--- src/_251_300/_265_PaintHouse.java
+++ src/_251_300/_265_PaintHouse.java
@@ -18,7 +18,90 @@ package _251_300;
  * Could you solve it in O(nk) runtime?
  */
 public class _265_PaintHouse {
+    /**
+     * 3ms
+     * T = n * k
+     * S = k
+     * https://www.jiuzhang.com/solution/paint-house-ii/
+     * https://www.acwing.com/solution/content/8126/
+     */
     public int minCostII(int[][] costs) {
-        return -1;
+
+        int n = costs.length;
+
+        if (n == 0) {
+
+            return 0;
+
+        }
+
+        int k = costs[0].length;
+
+        //dp[i][j]表示第i幢房子涂j的颜色最小的总和
+
+        //初始化状态dp[0][i]=costs[0][i]
+
+        int[][] dp = new int[2][k];
+
+        for (int i = 0; i < k; i++) {
+
+            dp[0][i] = costs[0][i];
+
+        }
+
+        int mi, sec_mi;
+
+        for (int i = 1; i < n; i++) {
+
+            //mi表示最小值，sec_mi表示次小值
+
+            mi = Integer.MAX_VALUE;
+
+            sec_mi = Integer.MAX_VALUE;
+
+            for (int j = 0; j < k; j++) {
+
+                if (dp[i & 1 ^ 1][j] <= mi) {
+
+                    sec_mi = mi;
+
+                    mi = dp[i & 1 ^ 1][j];
+
+                } else if (dp[i & 1 ^ 1][j] < sec_mi) {
+
+                    sec_mi = dp[i & 1 ^ 1][j];
+
+                }
+
+            }
+
+            for (int j = 0; j < k; j++) {
+
+                //若最小值与当前要涂的颜色相同
+
+                if (dp[i & 1 ^ 1][j] == mi) {
+
+                    dp[i & 1][j] = sec_mi + costs[i][j];
+
+                } else {
+
+                    dp[i & 1][j] = mi + costs[i][j];
+
+                }
+
+            }
+
+        }
+
+        mi = Integer.MAX_VALUE;
+
+        for (int j = 0; j < k; j++) {
+
+            mi = Math.min(mi, dp[n & 1 ^ 1][j]);
+
+        }
+
+        return mi;
+
     }
 }
