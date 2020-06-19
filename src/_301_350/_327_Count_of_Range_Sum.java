1f6bd1666b29ba89b16c94142fcdbae291a7bbcb
LiFangCoding <fanglihust@gmail.com>
2020-06-19 21:31:41 +0900

    add todo 327

---
+++ src/_301_350/_327_Count_of_Range_Sum.java
@@ -0,0 +1,26 @@
+package _301_350;
+
+/**
+ * Given an integer array nums, return the number of range sums that lie in [lower, upper] inclusive.
+ * Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
+ * <p>
+ * Note:
+ * A naive algorithm of O(n2) is trivial. You MUST do better than that.
+ * <p>
+ * Example:
+ * <p>
+ * Input: nums = [-2,5,-1], lower = -2, upper = 2,
+ * Output: 3
+ * Explanation: The three ranges are : [0,0], [2,2], [0,2] and their respective sums are: -2, -1, 2.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-of-range-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _327_Count_of_Range_Sum {
+    /**
+     * T = n * logn
+     * 树状数组
+     */
+    //TODO
+}
