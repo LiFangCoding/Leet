c87c5ae0449ffc1710980b15736174b77a953c6b
LiFangCoding <fanglihust@gmail.com>
2020-06-19 18:47:23 +0900

    add template 321

---
+++ src/_301_350/_321_Create_Maximum_number.java
@@ -0,0 +1,38 @@
+package _301_350;
+
+/**
+ * Given two arrays of length m and n with digits 0-9 representing two numbers. Create the maximum number of length k <= m + n from digits of the two. The relative order of the digits from the same array must be preserved. Return an array of the k digits.
+ * <p>
+ * Note: You should try to optimize your time and space complexity.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * nums1 = [3, 4, 6, 5]
+ * nums2 = [9, 1, 2, 5, 8, 3]
+ * k = 5
+ * Output:
+ * [9, 8, 6, 5, 3]
+ * Example 2:
+ * <p>
+ * Input:
+ * nums1 = [6, 7]
+ * nums2 = [6, 0, 4]
+ * k = 5
+ * Output:
+ * [6, 7, 6, 0, 4]
+ * Example 3:
+ * <p>
+ * Input:
+ * nums1 = [3, 9]
+ * nums2 = [8, 9]
+ * k = 3
+ * Output:
+ * [9, 8, 9]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/create-maximum-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _321_Create_Maximum_number {
+}
