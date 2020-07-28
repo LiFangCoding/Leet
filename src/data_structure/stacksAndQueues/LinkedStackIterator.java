b8b39cebb8dda4735bfbf95b8361ff62e4f3e8bf
LiFangCoding <fanglihust@gmail.com>
2020-07-28 17:15:22 +0900

    finish stack and queue

---
+++ src/data_structure/stacksAndQueues/LinkedStackIterator.java
@@ -0,0 +1,48 @@
+package data_structure.stacksAndQueues;
+
+import java.util.Iterator;
+
+public class LinkedStackIterator<Item> implements Iterable<Item> {
+    private Node first = null;
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        return item;
+    }
+}
