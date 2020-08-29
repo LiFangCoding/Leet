9eb5c0b4e088bd8766467f2080be66f8e4edf12e
LiFangCoding <fanglihust@gmail.com>
2020-08-30 01:46:05 +0900

    refactor 235

--- src/_201_250/_235_LowestCommonAncestor_of_BST.java
+++ src/_201_250/_235_LowestCommonAncestor_of_BST.java
@@ -30,27 +30,51 @@ import common.TreeNode;
  * p and q are different and both values will exist in the BST.
  */
 public class _235_LowestCommonAncestor_of_BST {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
+    class Sol_loop {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+
+            TreeNode cur = root;
+            int small = Math.min(p.val, q.val);
+            int large = Math.max(p.val, q.val);
+            /**
+             * !!! need a while
+             */
+            while (cur != null) {
+                if (cur.val >= small && cur.val <= large) {
+                    return cur;
+                } else if (cur.val < small) {
+                    cur = cur.right;
+                } else {
+                    cur = cur.left;
+                }
+            }
+
             return null;
         }
-
-        TreeNode cur = root;
-        int small = Math.min(p.val, q.val);
-        int large = Math.max(p.val, q.val);
-        /**
-         * !!! need a while
-         */
-        while (cur != null) {
-            if (cur.val >= small && cur.val <= large) {
-                return cur;
-            } else if (cur.val < small) {
-                cur = cur.right;
-            } else {
-                cur = cur.left;
-            }
-        }
-
-        return null;
     }
+
+    class Sol_rec {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (p.val > q.val) {
+                TreeNode temp = p;
+                p = q;
+                q = temp;
+            }
+
+            // p < q.
+            if (p.val <= root.val && q.val >= root.val) {
+                return root;
+            }
+
+            if (q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            }
+
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
+
 }
