a9b179119ed60144b4f75b062b1a8b57cd5f3248
LiFangCoding <fanglihust@gmail.com>
2020-09-02 01:40:44 +0900

    refactor 229

--- src/_201_250/_229_MajorityElement2.java
+++ src/_201_250/_229_MajorityElement2.java
@@ -18,6 +18,46 @@ import java.util.List;
  * Output: [1,2]
  */
 public class _229_MajorityElement2 {
+
+    class Sol_acwing {
+        public List<Integer> majorityElement(int[] nums) {
+            int r1 = 0, r2 = 0, c1 = 0, c2 = 0;
+
+            for (int x : nums) {
+                if (c1 != 0 && x == r1) {
+                    c1++;
+                } else if (c2 != 0 && x == r2) {
+                    c2++;
+                } else if (c1 == 0) {
+                    r1 = x;
+                    c1++;
+                } else if (c2 == 0) {
+                    r2 = x;
+                    c2++;
+                } else {
+                    c1--;
+                    c2--;
+                }
+            }
+
+            c1 = 0;
+            c2 = 0;
+            for (int x : nums) {
+                if (x == r1) {
+                    c1++;
+                } else if (x == r2) {
+                    c2++;
+                }
+            }
+
+            int n = nums.length;
+            List<Integer> res = new ArrayList<>();
+            if (c1 > n / 3) res.add(r1);
+            if (c2 > n / 3) res.add(r2);
+            return res;
+        }
+    }
+
     public static void main(String[] args) {
         _229_MajorityElement2 test = new _229_MajorityElement2();
         int[] A = new int[]{3, 2, 3};
