49537eaaa7f7e94b854ab6f44564086f074fc7ed
LiFangCoding <fanglihust@gmail.com>
2020-09-06 18:11:41 +0900

    refactor 205

--- src/_201_250/_205_IsomorphicStrings.java
+++ src/_201_250/_205_IsomorphicStrings.java
@@ -25,35 +25,18 @@ import java.util.Map;
  */
 public class _205_IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
-        int len = s.length();
+        if (s.length() != t.length()) return false;
 
-        /**
-         * from s char -> t char. one -> one map
-         */
         Map<Character, Character> st = new HashMap<>();
-        /**
-         * check if t char duplicates
-         */
         Map<Character, Character> ts = new HashMap<>();
 
-        for (int i = 0; i < len; i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-
-            if (st.containsKey(c1)) {
-                if (st.get(c1) != c2) {
-                    return false;
-                }
-            } else if (ts.containsKey(c2)) {
-                if (ts.get(c2) != c1) {
-                    return false;
-                }
-            } else {
-                st.put(c1, c2);
-                ts.put(c2, c1);
-            }
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i), b = t.charAt(i);
+            if (st.containsKey(a) && st.get(a) != b) return false;
+            st.put(a, b);
+            if (ts.containsKey(b) && ts.get(b) != a) return false;
+            ts.put(b, a);
         }
-
         return true;
     }
 }
