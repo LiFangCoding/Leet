401474a97094ec2ed8f832d752260cca1eff5d59
LiFangCoding <fanglihust@gmail.com>
2020-09-07 23:04:34 +0900

    refactor 213

--- src/_201_250/_213_HouseRobber2.java
+++ src/_201_250/_213_HouseRobber2.java
@@ -1,7 +1,5 @@
 package _201_250;
 
-import java.util.Arrays;
-
 /**
  * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
  * <p>
@@ -21,42 +19,31 @@ import java.util.Arrays;
  *              Total amount you can rob = 1 + 3 = 4.
  */
 public class _213_HouseRobber2 {
-    public int rob(int[] A) {
-        if (A == null || A.length == 0) {
-            return 0;
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        // important
+        if (n == 1) return nums[0];
+
+        int[] f = new int[n + 1];
+        int[] g = new int[n + 1];
+
+        // do not select idx 0
+        for (int i = 2; i <= n; i++) {
+            f[i] = g[i - 1] + nums[i - 1];
+            g[i] = Math.max(f[i - 1], g[i - 1]);
         }
+        int res = Math.max(f[n], g[n]);
 
-        if (A.length == 1) {
-            return A[0];
+        // must select idx 0
+        f[1] = nums[0];
+        g[1] = Integer.MIN_VALUE;
+        for (int i = 2; i <= n; i++) {
+            f[i] = g[i - 1] + nums[i - 1];
+            g[i] = Math.max(f[i - 1], g[i - 1]);
         }
-        int len = A.length;
-        int ans1 = robOneWay(Arrays.copyOfRange(A, 0, len - 1));
-        int ans2 = robOneWay(Arrays.copyOfRange(A, 1, len));
-        return Math.max(ans1, ans2);
-    }
-
-    public int robOneWay(int[] A) {
-        if (A == null || A.length == 0) {
-            return 0;
-        }
-
-        int len = A.length;
-        //!!!
-        if (len == 1) {
-            return A[0];
-        }
-
-        /**
-         * f[i] means until the ith index, the maximum rob
-         */
-        int[] f = new int[len];
-        f[0] = A[0];
-        f[1] = Math.max(A[0], A[1]);
-
-        for (int i = 2; i < len; i++) {
-            f[i] = Math.max(f[i - 2] + A[i], f[i - 1]);
-        }
-
-        return f[len - 1];
+        res = Math.max(res, g[n]);
+        return res;
     }
 }
