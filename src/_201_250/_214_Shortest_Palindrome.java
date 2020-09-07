f6a2a19bdb6bdfbeca3c294e5790d06e5cd3fb3d
LiFangCoding <fanglihust@gmail.com>
2020-09-07 23:37:58 +0900

    refactor 214

--- src/_201_250/_214_Shortest_Palindrome.java
+++ src/_201_250/_214_Shortest_Palindrome.java
@@ -13,40 +13,26 @@ package _201_250;
  * Output: "dcbabcd"
  */
 public class _214_Shortest_Palindrome {
-    /**
-     * TODO
-     * empty string
-     *
-     * @param s
-     * @return
-     */
     public String shortestPalindrome(String s) {
-      int j = 0;
+        int n = s.length();
 
-      for (int i = s.length() - 1; i >= 0; i--) {//找到第一个使他不回文的位置
+        String t = new StringBuilder(s).reverse().toString();
+        s = ' ' + s + '#' + t;
+        char[] sa = s.toCharArray();
 
-        if (s.charAt(i) == s.charAt(j)) {
-
-          j += 1;
-
+        int[] ne = new int[n * 2 + 2];
+        for (int i = 2, j = 0; i <= n * 2 + 1; i++) {
+            while (j != 0 && sa[i] != sa[j + 1]) j = ne[j];
+            if (sa[i] == sa[j + 1]) j++;
+            ne[i] = j;
         }
 
-      }
-
-      if (j == s.length()) {  //本身是回文
-
-        return s;
-
-      }
-
-      String suffix = s.substring(j); // 后缀不能够匹配的字符串
-
-      String prefix = new StringBuilder(suffix).reverse().toString(); // 前面补充prefix让他和suffix回文匹配
-
-      String mid = shortestPalindrome(s.substring(0, j)); //递归调用找 [0,j]要最少可以补充多少个字符让他回文
-
-      String ans = prefix + mid + suffix;
-
-      return ans;
+        int len = ne[n * 2 + 1];
+        String left = s.substring(1, len + 1), right = s.substring(len + 1, n + 1);
+        StringBuilder sb = new StringBuilder();
+        sb.append(right).reverse();
+        sb.append(left);
+        sb.append(right);
+        return sb.toString();
     }
 }
