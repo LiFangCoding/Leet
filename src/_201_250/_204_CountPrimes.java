bcdfb0d4067a5b2f7331011e99d621baeaa64a9f
LiFangCoding <fanglihust@gmail.com>
2020-09-09 19:23:23 +0900

    refactor 204

--- src/_201_250/_204_CountPrimes.java
+++ src/_201_250/_204_CountPrimes.java
@@ -1,6 +1,8 @@
 package _201_250;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Count the number of prime numbers less than a non-negative number, n.
@@ -12,6 +14,29 @@ import java.util.Arrays;
  * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
  */
 public class _204_CountPrimes {
+    class Sol_acwing_N {
+        public int countPrimes(int n) {
+            boolean[] st = new boolean[n + 1];
+            List<Integer> primes = new ArrayList<>();
+
+            for (int i = 2; i < n; i++) {
+                if (!st[i])
+                    primes.add(i);
+                System.out.println("i is " + i);
+                System.out.println(primes);
+
+                for (int j = 0; i * primes.get(j) < n; j++) {
+                    System.out.println(String.format("the value marked compound is %d %n", i * primes.get(j)));
+                    st[i * primes.get(j)] = true;
+                    if (i % primes.get(j) == 0)
+                        break;
+                }
+            }
+
+            return primes.size();
+        }
+    }
+
     public int countPrimes_faster(int n) {
         boolean[] isPrim = new boolean[n];
         Arrays.fill(isPrim, true);
