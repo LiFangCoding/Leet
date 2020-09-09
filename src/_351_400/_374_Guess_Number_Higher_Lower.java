0857633fe3ca4fde3d72e46d469ca10bffa5b1f6
LiFangCoding <fanglihust@gmail.com>
2020-09-10 01:35:48 +0900

    solve 374

---
+++ src/_351_400/_374_Guess_Number_Higher_Lower.java
@@ -0,0 +1,51 @@
+package _351_400;
+
+/**
+ * We are playing the Guess Game. The game is as follows:
+ * <p>
+ * I pick a number from 1 to n. You have to guess which number I picked.
+ * <p>
+ * Every time you guess wrong, I'll tell you whether the number is higher or lower.
+ * <p>
+ * You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+ * <p>
+ * -1 : My number is lower
+ * 1 : My number is higher
+ * 0 : Congrats! You got it!
+ * Example :
+ * <p>
+ * Input: n = 10, pick = 6
+ * Output: 6
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/guess-number-higher-or-lower
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _374_Guess_Number_Higher_Lower {
+    public int guessNumber(int n) {
+        int l = 1;
+        int r = n;
+
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (guess(mid) == 0)
+                return mid;
+            else if (guess(mid) == -1)
+                r = mid;
+            else
+                l = mid;
+            // System.out.println("guess mid is " + guess(mid) + " mid is " + mid);
+        }
+
+        if (guess(l) == 0)
+            return l;
+        if (guess(r) == 0)
+            return r;
+        return -1;
+    }
+
+    // api
+    public int guess(int l) {
+        return -1;
+    }
+}
