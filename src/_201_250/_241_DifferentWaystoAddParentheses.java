cde3e30cf3491cb902e64010613f99e4a8aaacd5
LiFangCoding <fanglihust@gmail.com>
2020-09-11 01:33:52 +0900

    refactor 241

--- src/_201_250/_241_DifferentWaystoAddParentheses.java
+++ src/_201_250/_241_DifferentWaystoAddParentheses.java
@@ -25,49 +25,57 @@ import java.util.List;
  * (((2*3)-4)*5) = 10
  */
 public class _241_DifferentWaystoAddParentheses {
-  //TODO: more practice
+    /**
+     * 2ms
+     * T = Cn  卡特兰数
+     * 采用递归的方式，每次通过某个运算符，将当前字符串分成左右两部分，递归子问题计算。
+     * 然后根据左右两部分返回的结果，通过该运算符组合成新的结果。
+     * 递归出口为当前字符串仅有一个数字。
+     */
+    List<String> expr;
 
-  /**
-   * 2ms
-   * T = Cn  卡特兰数
-   * 采用递归的方式，每次通过某个运算符，将当前字符串分成左右两部分，递归子问题计算。
-   * 然后根据左右两部分返回的结果，通过该运算符组合成新的结果。
-   * 递归出口为当前字符串仅有一个数字。
-   */
-  public List<Integer> diffWaysToCompute(String input) {
-    List<Integer> ans = new ArrayList<>();
+    public List<Integer> diffWaysToCompute(String input) {
+        expr = new ArrayList<>();
+        char[] s = input.toCharArray();
 
-    for (int i = 0; i < input.length(); i++) {
-      if (input.charAt(i) == '-' || input.charAt(i) == '*' || input.charAt(i) == '+') {
-        String part1 = input.substring(0, i);
-        String part2 = input.substring(i + 1);
-        List<Integer> part1Ans = diffWaysToCompute(part1);
-        List<Integer> part2Ans = diffWaysToCompute(part2);
-
-        for (Integer p1 : part1Ans) {
-          for (Integer p2 : part2Ans) {
-            int c = 0;
-            switch (input.charAt(i)) {
-            case '+':
-              c = p1 + p2;
-              break;
-            case '-':
-              c = p1 - p2;
-              break;
-            case '*':
-              c = p1 * p2;
-              break;
+        for (int i = 0; i < s.length; i++) {
+            if (Character.isDigit(s[i])) {
+                int j = i, x = 0;
+                while (j < s.length && Character.isDigit(s[j])) x = x * 10 + (s[j++] - '0');
+                i = j - 1;
+                expr.add(String.valueOf(x));
+            } else {
+                expr.add("" + s[i]);
             }
-            ans.add(c);
-          }
         }
-      }
+        return dfs(0, expr.size() - 1);
     }
 
-    if (ans.size() == 0) {
-      ans.add(Integer.valueOf(input));
+    List<Integer> dfs(int l, int r) {
+        List<Integer> res = new ArrayList<>();
+
+        if (l == r) {
+            res.add(Integer.parseInt(expr.get(l)));
+            return res;
+        }
+
+        for (int i = l + 1; i < r; i += 2) {
+            List<Integer> left = dfs(l, i - 1);
+            List<Integer> right = dfs(i + 1, r);
+
+            for (int x : left) {
+                for (int y : right) {
+                    int z = 0;
+                    String ope = expr.get(i);
+                    if (ope.equals("+")) z = x + y;
+                    else if (ope.equals("-")) z = x - y;
+                    else z = x * y;
+                    res.add(z);
+                }
+            }
+        }
+
+        return res;
     }
 
-    return ans;
-  }
 }
