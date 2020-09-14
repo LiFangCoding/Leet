142a37c7529820c812e30d3142377086bb54be98
LiFangCoding <fanglihust@gmail.com>
2020-09-14 10:15:59 +0900

    refactor 7

--- src/_1_50/_7_ReverseInteger.java
+++ src/_1_50/_7_ReverseInteger.java
@@ -24,27 +24,21 @@ public class _7_ReverseInteger {
      * O(log10(x)). Because int型整数在十进制表示下最多有10位，对于每一位的计算量是常数级的.
      * Overflow  [-2^31, 2^31 - 1]
      */
-    public static int reverse(int n) {
-        boolean positive = n > 0;
-        int ans = 0;
-
-        while (n != 0) {
-            // -12 % 10 == -2
-            int digit = n % 10;
-            n = n / 10;
-
-            if (positive && ans > (Integer.MAX_VALUE - digit) / 10) {
+    public int reverse(int x) {
+        int r = 0;
+        // 123 % 10 = 3, -123 % 10 = -3. for negative
+        while (x != 0) {
+            int d = x % 10;
+            // 10r + d > max
+            if (r > 0 && r > (Integer.MAX_VALUE - d) / 10)
                 return 0;
-            }
-
-            if (!positive && ans < (Integer.MIN_VALUE - digit) / 10) {
+            // 10r + d < min
+            if (r < 0 && r < (Integer.MIN_VALUE - d) / 10)
                 return 0;
-            }
-
-            ans = ans * 10 + digit;
+            r = r * 10 + d;
+            x /= 10;
         }
-
-        return ans;
+        return r;
     }
 
     public static void main(String[] args) {
