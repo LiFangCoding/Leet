f4595b63079011b43db78dbe3e6d13a61c955152
LiFangCoding <fanglihust@gmail.com>
2020-09-14 13:40:08 +0900

    refactor 13,14

--- src/_1_50/_12_IntegerToRoman.java
+++ src/_1_50/_12_IntegerToRoman.java
@@ -54,15 +54,14 @@ public class _12_IntegerToRoman {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        StringBuilder ans = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < values.length; i++) {
-            int count = num / values[i];
-            for (int j = 0; j < count; j++) {
-                ans.append(romans[i]);
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(romans[i]);
             }
-            num %= values[i];
         }
 
-        return ans.toString();
+        return sb.toString();
     }
 }
--- src/_1_50/_13_RomanToInteger.java
+++ src/_1_50/_13_RomanToInteger.java
@@ -57,21 +57,22 @@ public class _13_RomanToInteger {
         map.put('D', 500);
         map.put('M', 1000);
 
-        int ans = 0;
+        int res = 0;
         int len = s.length();
-        char[] A = s.toCharArray();
+        char[] sa = s.toCharArray();
         /**
          * This is when to add or substract.
          */
         for (int i = 0; i < len; i++) {
-            if (i + 1 < len && map.get(A[i]) < map.get(A[i + 1])) {
-                ans += map.get(A[i + 1]) - map.get(A[i]);
-                i++;
-            } else  {
-                ans += map.get(A[i]);
+            if (i + 1 < len && map.get(sa[i]) < map.get(sa[i + 1])) {
+                res -= map.get(sa[i]);
+                //                res += map.get(A[i + 1]) - map.get(A[i]);
+                //                i++;
+            } else {
+                res += map.get(sa[i]);
             }
         }
 
-        return ans;
+        return res;
     }
 }
