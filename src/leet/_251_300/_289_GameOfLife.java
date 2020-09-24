b9c1041e8ee908d068975d680ce51105c7cb3a05
LiFangCoding <fanglihust@gmail.com>
2020-09-25 00:14:06 +0900

    refactor 289

--- src/leet/_251_300/_289_GameOfLife.java
+++ src/leet/_251_300/_289_GameOfLife.java
@@ -33,29 +33,72 @@ package leet._251_300;
  * In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
  */
 public class _289_GameOfLife {
+    class Sol_ac {
+        public void gameOfLife(int[][] board) {
+            // n rows, m cols
+            if (board.length == 0 || board[0].length == 0) {
+                return;
+            }
+
+            // live1, dead 0. use the second digit to store
+            int n = board.length, m = board[0].length;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    int live = 0;
+
+                    for (int x = Math.max(0, i - 1); x <= Math.min(n - 1, i + 1); x++) {
+                        for (int y = Math.max(0, j - 1); y <= Math.min(m - 1, j + 1); y++) {
+                            if (x == i && y == j) continue;
+                            if ((board[x][y] & 1) == 1) {
+                                live++;
+                            }
+                        }
+                    }
+                    int cur = board[i][j] & 1, next = 0;
+                    if (cur == 1) {
+                        if (live < 2 || live > 3) next = 0;
+                        else next = 1;
+                    } else {
+                        if (live == 3) next = 1;
+                        else next = 0;
+                    }
+                    // System.out.println("i is " + i + ", j is " + j + ", val is " + board[i][j]);
+                    board[i][j] |= next << 1;
+                    // System.out.println("i is " + i + ", j is " + j + ", val is " + board[i][j]);
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    board[i][j] >>= 1;
+                }
+            }
+        }
+    }
 
     class Sol_inPlace {
         // before live and after is live 2.
         int live = 1, die = 0, liveTodie = 2, dieToLive = -1;
-        int[] dx = { -1, 1, 0, 0, -1, -1, 1, 1 };
-        int[] dy = { 0, 0, 1, -1, 1, -1, 1, -1 };
+        int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+        int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
 
         public void gameOfLife(int[][] board) {
             if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) {
                 return;
             }
 
-            int rows = board.length;
-            int cols = board[0].length;
+            int m = board.length;
+            int n = board[0].length;
 
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     board[i][j] = getVal(board, i, j);
                 }
             }
 
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     board[i][j] = (board[i][j] == live || board[i][j] == dieToLive) ? live : die;
                 }
             }
---
+++ src/leet/_351_400/_387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,5 @@
+package leet._351_400;
+
+public class _387_First_Unique_Character_in_a_String {
+
+}
