bf66cc9c86d97c5bd7e00f255aa6a32644909fe1
LiFangCoding <fanglihust@gmail.com>
2020-09-30 23:40:08 +0900

    refactor 338

--- src/leet/_301_350/_338_Counting_Bits.java
+++ src/leet/_301_350/_338_Counting_Bits.java
@@ -25,26 +25,12 @@ public class _338_Counting_Bits {
      * https://leetcode-cn.com/problems/counting-bits/solution/hen-qing-xi-de-si-lu-by-duadua/
      */
     class Sol_OddEven {
-        /**
-         * 很简单啊，不用多说，设dp[i]为i的二进制形式的1的个数，i是奇数时，dp[i]=dp[i-1]+1,因为i是在i-1的二进制数上加了个1啊；i是偶数时，dp[i]=dp[i/2],因为i就是把i/2往左移（是数左移，末尾补0）得到的，所以1的个数没变
-         * <p>
-         * 作者：jin-ai-yi
-         * 链接：https://leetcode-cn.com/problems/counting-bits/solution/zhe-ying-gai-shi-dao-easyti-ba-wei-yun-suan-ji-chu/
-         * 来源：力扣（LeetCode）
-         */
-
+        // f[i] = f[i >> 1] + (i & 1)
         public int[] countBits(int num) {
             int[] f = new int[num + 1];
-
-            f[0] = 0;
             for (int i = 1; i <= num; i++) {
-                if (i % 2 == 1) {
-                    f[i] = f[i - 1] + 1;
-                } else {
-                    f[i] = f[i / 2];
-                }
+                f[i] = f[i >> 1] + (i & 1);
             }
-
             return f;
         }
     }
