24f7260b5b248c2d666e1942db87688cab11c678
LiFangCoding <fanglihust@gmail.com>
2020-10-01 00:02:11 +0900

    refactor 342

--- src/leet/_301_350/_342_Power_of_Four.java
+++ src/leet/_301_350/_342_Power_of_Four.java
@@ -18,8 +18,22 @@ package leet._301_350;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class _342_Power_of_Four {
+
+    class Sol_easier {
+        // 4 ^ k, means 2 ^ 2k = (2 ^ k) ^ 2
+        // 1 n > 0
+        // 2 n only contains factor 2
+        // 3
+        public boolean isPowerOfFour(int num) {
+            if (num <= 0) return false;
+            int r = (int) Math.sqrt(num);
+            if (r * r != num) return false;
+            return (num & (num - 1)) == 0;
+        }
+    }
+
     /**
-     * (数论) O(1)O(1)
+     * (数论) O(1)
      * 这道题目类似于LeetCode 326. Power of Three。
      * <p>
      * n 是4的整数次幂，等价于 n 是平方数，且 n 的质因子只有2。
@@ -28,15 +42,19 @@ public class _342_Power_of_Four {
      * <p>
      * 时间复杂度分析：只有常数次计算，所以时间复杂度是 O(1)
      *
-     * @param num
+     * @param
      * @return
      */
-    public boolean isPowerOfFour(int num) {
-        if (num <= 0) {
-            return false;
-        }
+    class Sol_1 {
+        public boolean isPowerOfFour(int num) {
+            if (num <= 0) {
+                return false;
+            }
 
-        int t = (int) Math.sqrt(num);
-        return t * t == num && ((1 << 30) % num == 0);
+            int t = (int) Math.sqrt(num);
+            return t * t == num && ((1 << 30) % num == 0);
+        }
     }
+
+
 }
