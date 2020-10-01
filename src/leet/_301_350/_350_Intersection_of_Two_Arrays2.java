ad48595c4a905d00f5a8b4ee11e6b57c94e050bf
LiFangCoding <fanglihust@gmail.com>
2020-10-02 00:45:46 +0900

    refactor 350

--- src/leet/_301_350/_350_Intersection_of_Two_Arrays2.java
+++ src/leet/_301_350/_350_Intersection_of_Two_Arrays2.java
@@ -1,10 +1,6 @@
 package leet._301_350;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Given two arrays, write a function to compute their intersection.
@@ -28,72 +24,60 @@ import java.util.Map;
  * What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
  */
 public class _350_Intersection_of_Two_Arrays2 {
-    public int[] intersect(int[] A1, int[] A2) {
-        if (A1 == null || A2 == null) {
-            return new int[0];
-        }
-        // num -> count
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int num : A1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        List<Integer> anslist = new ArrayList<>();
-
-        for (int num : A2) {
-            if (!map.containsKey(num)) {
-                continue;
+    class Sol_map_n {
+        public int[] intersect(int[] nums1, int[] nums2) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int x : nums1) {
+                map.put(x, map.getOrDefault(x, 0) + 1);
             }
 
-            if (map.get(num) > 0) {
-                anslist.add(num);
-                map.put(num, map.get(num) - 1);
+            List<Integer> list = new ArrayList<>();
+            for (int x : nums2) {
+                if (map.containsKey(x) && map.get(x) > 0) {
+                    list.add(x);
+                    map.put(x, map.get(x) - 1);
+                }
             }
+            int[] res = new int[list.size()];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = list.get(i);
+            }
+            return res;
         }
-
-        int[] ans = new int[anslist.size()];
-        for (int i = 0; i < anslist.size(); i++) {
-            ans[i] = anslist.get(i);
-        }
-
-        return ans;
     }
 
-    public int[] intersect_two_pointers(int[] A1, int[] A2) {
-        if (A1 == null || A2 == null) {
-            return new int[0];
-        }
-
-        Arrays.sort(A1);
-        Arrays.sort(A2);
-
-        int idx1 = 0;
-        int len1 = A1.length;
-        int idx2 = 0;
-        int len2 = A2.length;
-
-        List<Integer> anslist = new ArrayList<>();
-        while (idx1 < len1 && idx2 < len2) {
-            if (A1[idx1] == A2[idx2]) {
-                anslist.add(A1[idx1]);
-                idx1++;
-                idx2++;
-            } else if (A1[idx1] < A2[idx2]) {
-                idx1++;
-            } else {
-                idx2++;
+    class Sol_two_pointers_nlogn {
+        public int[] intersect_two_pointers(int[] A1, int[] A2) {
+            if (A1 == null || A2 == null) {
+                return new int[0];
             }
-        }
 
-        int[] ans = new int[anslist.size()];
-        for (int i = 0; i < anslist.size(); i++) {
-            ans[i] = anslist.get(i);
-        }
-        return ans;
-    }
+            Arrays.sort(A1);
+            Arrays.sort(A2);
 
-    public static void main(String[] args) {
-        System.out.println((Integer.MIN_VALUE - 1));
+            int idx1 = 0;
+            int len1 = A1.length;
+            int idx2 = 0;
+            int len2 = A2.length;
+
+            List<Integer> list = new ArrayList<>();
+            while (idx1 < len1 && idx2 < len2) {
+                if (A1[idx1] == A2[idx2]) {
+                    list.add(A1[idx1]);
+                    idx1++;
+                    idx2++;
+                } else if (A1[idx1] < A2[idx2]) {
+                    idx1++;
+                } else {
+                    idx2++;
+                }
+            }
+
+            int[] ans = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                ans[i] = list.get(i);
+            }
+            return ans;
+        }
     }
 }
