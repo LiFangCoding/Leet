7c7461a8ef7072fec36ce0fc48d02c3aa22b22c2
LiFangCoding <fanglihust@gmail.com>
2020-10-01 20:28:05 +0900

    refactor 337

--- src/leet/_301_350/_337_HouseRobber3.java
+++ src/leet/_301_350/_337_HouseRobber3.java
@@ -33,25 +33,25 @@ import common.TreeNode;
  * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
  */
 public class _337_HouseRobber3 {
+    // 0 means not chosen, 1 means chosen
+    // f[u][0] = Math.max(f(x, 0), f(x, 1)) + Math.max(f(y, 0) + f(y , 1));
+    // f[u][1] = f(x, 0) + f(y, 0) + v[u]
+    // ans = max(f(root, 0), f(root, 1));
     public int rob(TreeNode root) {
-        int[] ans = robOrNot(root);
-        return Math.max(ans[0], ans[1]);
+        int[] f = dfs(root);
+        return Math.max(f[0], f[1]);
     }
 
-    // 0 means do not rob. 1 means rob
-    // f(root)[0] = {f(root.left) 0, 1} + {f(root.right) 0, 1}
-    // f(root)1 = root.val + f(root.left)[0] + f[root.right][0]
-    public int[] robOrNot(TreeNode root) {
-        if (root == null) {
-            return new int[] { 0, 0 };
+    int[] dfs(TreeNode u) {
+        if (u == null) {
+            return new int[]{0, 0};
         }
 
-        int[] l = robOrNot(root.left);
-        int[] r = robOrNot(root.right);
-
-        int[] ans = new int[2];
-        ans[0] = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
-        ans[1] = root.val + l[0] + r[0];
-        return ans;
+        int[] x = dfs(u.left);
+        int[] y = dfs(u.right);
+        int[] f = new int[2];
+        f[0] = Math.max(x[0], x[1]) + Math.max(y[0], y[1]);
+        f[1] = u.val + x[0] + y[0];
+        return f;
     }
 }
