9dd2ffb20baf53fd96100fb68e627912945cbf88
LiFangCoding <fanglihust@gmail.com>
2020-10-11 01:37:37 +0900

    solve 387

--- src/leet/_351_400/_387_First_Unique_Character_in_a_String.java
+++ src/leet/_351_400/_387_First_Unique_Character_in_a_String.java
@@ -1,5 +1,12 @@
 package leet._351_400;
 
 public class _387_First_Unique_Character_in_a_String {
-
+    public int firstUniqChar(String s) {
+        int[] map = new int[256];
+        for (char c : s.toCharArray()) map[c]++;
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i)] == 1) return i;
+        }
+        return -1;
+    }
 }
