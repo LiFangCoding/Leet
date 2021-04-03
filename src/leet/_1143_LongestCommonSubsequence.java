f592ef130d5160c1aff84d6b7fef3c869713e0d3
LiFangCoding <fanglihust@gmail.com>
2021-04-03 21:02:47 +0900

    refactor

--- src/leet/_1143_LongestCommonSubsequence.java
+++ src/leet/_1143_LongestCommonSubsequence.java
@@ -35,20 +35,28 @@ package leet;
  * The input strings consist of lowercase English characters only.
  */
 public class _1143_LongestCommonSubsequence {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length(), n = text2.length();
+    public int longestCommonSubsequence(String s1, String s2) {
+        // f[i][j] means [0, i - 1], [0, j - 1] the longest commmon sub
+        // f[0][j] = 0, f[i][0] = 0
+        // f[i][j] = max (f[i - 1][j - 1] + 1 if c1 == c2, f[i - 1][j], f[i][j - 1])
 
-        int[][] dp = new int[m + 1][n + 1];
+        int m = s1.length(), n = s2.length();
+        int[][] f = new int[m + 1][n + 1];
+        int res = 0;
+
         for (int i = 1; i <= m; i++) {
+            char c1 = s1.charAt(i - 1);
             for (int j = 1; j <= n; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                char c2 = s2.charAt(j - 1);
+                if (c1 == c2) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    f[i][j] = Math.max(f[i - 1][j],f[i][j - 1]);
                 }
+                res = Math.max(res, f[i][j]);
             }
         }
 
-        return dp[m][n];
+        return res;
     }
 }
